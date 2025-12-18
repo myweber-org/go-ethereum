06f36287853622a8b4dbfb69420609128774ad1d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-18 18:35:27 +0000

    Implement user authentication with JWT token generation and validation

---
+++ log/user_authenticator.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+    "errors"
+    "time"
+
+    "github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+    Username string `json:"username"`
+    UserID   int    `json:"user_id"`
+    jwt.RegisteredClaims
+}
+
+var jwtKey = []byte("your_secret_key_here")
+
+func GenerateToken(username string, userID int) (string, error) {
+    expirationTime := time.Now().Add(24 * time.Hour)
+    claims := &Claims{
+        Username: username,
+        UserID:   userID,
+        RegisteredClaims: jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(expirationTime),
+            IssuedAt:  jwt.NewNumericDate(time.Now()),
+            Issuer:    "auth_service",
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(jwtKey)
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+    claims := &Claims{}
+    token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+        return jwtKey, nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    if !token.Valid {
+        return nil, errors.New("invalid token")
+    }
+
+    return claims, nil
+}
