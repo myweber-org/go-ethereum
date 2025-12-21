831b932117a7e4413709911f4458c6411e9f062f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-21 20:15:22 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- crypto/bn256/cloudflare/data_cleaner.go
+++ crypto/bn256/cloudflare/data_cleaner.go
@@ -61,3 +61,25 @@ func main() {
 	
 	fmt.Printf("Total unique items: %d\n", cleaner.GetUniqueCount())
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
