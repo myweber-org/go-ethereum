ca1d28f3d2a2159545f3f89110b168faec970462
InkspillVerse <inkspill.verse@outlook.com>
2025-12-25 16:09:05 +0000

    Add configuration loader with YAML parsing and validation support

---
+++ crypto/secp256k1/libsecp256k1/src/java/config_loader.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Driver   string `yaml:"driver"`
+		Host     string `yaml:"host"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		return nil, errors.New("config path cannot be empty")
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := validateConfig(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func validateConfig(cfg *Config) error {
+	if cfg.Server.Host == "" {
+		return errors.New("server host is required")
+	}
+	if cfg.Server.Port <= 0 || cfg.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if cfg.Database.Driver == "" {
+		return errors.New("database driver is required")
+	}
+	if cfg.Logging.Level == "" {
+		cfg.Logging.Level = "info"
+	}
+	return nil
+}
