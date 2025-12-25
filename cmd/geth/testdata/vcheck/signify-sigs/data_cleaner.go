e4883f69d3e77ee2bf7b7ca915acd8a3f898ec00
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-25 22:48:19 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- cmd/geth/testdata/vcheck/signify-sigs/data_cleaner.go
+++ cmd/geth/testdata/vcheck/signify-sigs/data_cleaner.go
@@ -27,3 +27,25 @@ func main() {
 	fmt.Println("Original:", strings)
 	fmt.Println("Unique:", uniqueStrings)
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
