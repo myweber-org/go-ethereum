2cb23a1088a432b35956b57ea002e55d28ec73ec
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-27 00:04:52 +0000

    Add log rotation with size-based file splitting and cleanup of old logs

--- cmd/evm/testdata/29/log_rotator.go
+++ cmd/evm/testdata/29/log_rotator.go
@@ -161,4 +161,115 @@ func main() {
     }
 
     fmt.Println("Log rotation completed")
+}package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sort"
+    "strconv"
+    "strings"
+    "time"
+)
+
+const (
+    maxFileSize  = 10 * 1024 * 1024 // 10MB
+    maxBackupCount = 5
+    logFileName   = "app.log"
+)
+
+type LogRotator struct {
+    currentSize int64
+    basePath    string
+}
+
+func NewLogRotator(basePath string) *LogRotator {
+    return &LogRotator{
+        basePath: basePath,
+    }
+}
+
+func (lr *LogRotator) Write(p []byte) (n int, err error) {
+    if lr.currentSize+int64(len(p)) > maxFileSize {
+        if err := lr.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    file, err := os.OpenFile(filepath.Join(lr.basePath, logFileName), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return 0, err
+    }
+    defer file.Close()
+
+    n, err = file.Write(p)
+    lr.currentSize += int64(n)
+    return n, err
+}
+
+func (lr *LogRotator) rotate() error {
+    currentPath := filepath.Join(lr.basePath, logFileName)
+    if _, err := os.Stat(currentPath); os.IsNotExist(err) {
+        return nil
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    backupPath := filepath.Join(lr.basePath, fmt.Sprintf("%s.%s", logFileName, timestamp))
+
+    if err := os.Rename(currentPath, backupPath); err != nil {
+        return err
+    }
+
+    lr.currentSize = 0
+    lr.cleanupOldBackups()
+    return nil
+}
+
+func (lr *LogRotator) cleanupOldBackups() {
+    files, err := filepath.Glob(filepath.Join(lr.basePath, logFileName+".*"))
+    if err != nil {
+        return
+    }
+
+    sort.Sort(sort.Reverse(sort.StringSlice(files)))
+
+    for i, file := range files {
+        if i >= maxBackupCount {
+            os.Remove(file)
+        }
+    }
+}
+
+func (lr *LogRotator) initialize() error {
+    filePath := filepath.Join(lr.basePath, logFileName)
+    info, err := os.Stat(filePath)
+    if os.IsNotExist(err) {
+        return nil
+    }
+    if err != nil {
+        return err
+    }
+
+    lr.currentSize = info.Size()
+    return nil
+}
+
+func main() {
+    rotator := NewLogRotator(".")
+    if err := rotator.initialize(); err != nil {
+        fmt.Printf("Initialization error: %v\n", err)
+        return
+    }
+
+    for i := 0; i < 1000; i++ {
+        logEntry := fmt.Sprintf("[%s] Log entry number %d\n", time.Now().Format(time.RFC3339), i)
+        if _, err := rotator.Write([]byte(logEntry)); err != nil {
+            fmt.Printf("Write error: %v\n", err)
+        }
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    fmt.Println("Log rotation test completed")
 }
