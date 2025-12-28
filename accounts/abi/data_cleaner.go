b3e8292ff024037f7d267991b11cf3aa03290402
InkspillVerse <inkspill.verse@outlook.com>
2025-12-28 22:00:46 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ accounts/abi/data_cleaner.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
