9e002866e3454417cd27e8314e27beadabea3001
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-28 16:27:51 +0000

    Add file encryption utility with AES-256-GCM implementation

--- crypto/secp256k1/libsecp256k1/sage/file_encryption.go
+++ crypto/secp256k1/libsecp256k1/sage/file_encryption.go
@@ -105,4 +105,100 @@ func main() {
 		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptData(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func generateRandomKey() ([]byte, error) {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func main() {
+	key, err := generateRandomKey()
+	if err != nil {
+		fmt.Printf("Key generation failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	originalData := []byte("Sensitive information requiring protection")
+	fmt.Printf("Original data: %s\n", originalData)
+
+	encrypted, err := encryptData(originalData, key)
+	if err != nil {
+		fmt.Printf("Encryption failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Encrypted data length: %d bytes\n", len(encrypted))
+
+	decrypted, err := decryptData(encrypted, key)
+	if err != nil {
+		fmt.Printf("Decryption failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Decrypted data: %s\n", decrypted)
+
+	if string(originalData) == string(decrypted) {
+		fmt.Println("Encryption/decryption successful")
+	} else {
+		fmt.Println("Encryption/decryption failed")
+		os.Exit(1)
+	}
 }
