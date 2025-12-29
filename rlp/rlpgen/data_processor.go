aa3c26722ad90584c1fc4b2dfdb0f911ccb62cd5
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-29 20:28:35 +0000

    Add data processing utility for cleaning and validating input strings

---
+++ rlp/rlpgen/data_processor.go
@@ -0,0 +1,39 @@
+
+package data_processor
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) ExtractDomain(email string) (string, bool) {
+	if !dp.ValidateEmail(email) {
+		return "", false
+	}
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 {
+		return "", false
+	}
+	return parts[1], true
+}
