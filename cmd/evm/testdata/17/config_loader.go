f76df3962b41cf95bbc5217b448c9243448cd9f9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-30 20:15:48 +0000

    Add configuration loader with YAML support and environment variable overrides

---
+++ cmd/evm/testdata/17/config_loader.go
@@ -0,0 +1,123 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host" env:"DB_HOST"`
+	Port     int    `yaml:"port" env:"DB_PORT"`
+	Username string `yaml:"username" env:"DB_USER"`
+	Password string `yaml:"password" env:"DB_PASS"`
+	Database string `yaml:"database" env:"DB_NAME"`
+}
+
+type ServerConfig struct {
+	Port         int    `yaml:"port" env:"SERVER_PORT"`
+	ReadTimeout  int    `yaml:"read_timeout" env:"READ_TIMEOUT"`
+	WriteTimeout int    `yaml:"write_timeout" env:"WRITE_TIMEOUT"`
+	DebugMode    bool   `yaml:"debug_mode" env:"DEBUG_MODE"`
+	LogLevel     string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+type AppConfig struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&config)
+
+	if err := validateConfig(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func overrideFromEnv(config *AppConfig) {
+	overrideString(&config.Server.LogLevel, "LOG_LEVEL")
+	overrideInt(&config.Server.Port, "SERVER_PORT")
+	overrideBool(&config.Server.DebugMode, "DEBUG_MODE")
+	overrideString(&config.Database.Host, "DB_HOST")
+	overrideInt(&config.Database.Port, "DB_PORT")
+	overrideString(&config.Database.Username, "DB_USER")
+	overrideString(&config.Database.Password, "DB_PASS")
+	overrideString(&config.Database.Database, "DB_NAME")
+}
+
+func overrideString(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
+	}
+}
+
+func overrideInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		var intVal int
+		if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+			*field = intVal
+		}
+	}
+}
+
+func overrideBool(field *bool, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val == "true" || val == "1" || val == "yes"
+	}
+}
+
+func validateConfig(config *AppConfig) error {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+
+	if config.Database.Host == "" {
+		return errors.New("database host is required")
+	}
+
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+
+	if config.Database.Database == "" {
+		return errors.New("database name is required")
+	}
+
+	validLogLevels := map[string]bool{
+		"debug": true,
+		"info":  true,
+		"warn":  true,
+		"error": true,
+		"fatal": true,
+	}
+
+	if !validLogLevels[config.Server.LogLevel] {
+		return errors.New("invalid log level")
+	}
+
+	return nil
+}
