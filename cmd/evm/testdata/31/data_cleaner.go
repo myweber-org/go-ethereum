6e161e7fd09f75e2bcbffe9bb27888f048bf327e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-31 16:33:15 +0000

    Add data cleaning utility to remove duplicate entries from a slice

---
+++ cmd/evm/testdata/31/data_cleaner.go
@@ -0,0 +1,13 @@
+package utils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
+}
