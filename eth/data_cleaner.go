0389b81ee4c361e1b120a2a09b7d077365d984d8
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-01 15:00:31 +0000

    Add data cleaning utility to remove duplicate entries from a slice

---
+++ eth/data_cleaner.go
@@ -0,0 +1,15 @@
+package datautils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
