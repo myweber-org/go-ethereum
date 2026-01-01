bd675a83fde62b48d2c23c324147705a08d7aebc
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-01 23:48:16 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- p2p/data_cleaner.go
+++ p2p/data_cleaner.go
@@ -59,3 +59,26 @@ func main() {
     fmt.Println("Original count:", len(sampleData))
     fmt.Println("Cleaned count:", len(result))
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := removeDuplicates(data)
+	fmt.Printf("Original: %v\n", data)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
