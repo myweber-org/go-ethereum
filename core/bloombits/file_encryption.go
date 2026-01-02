ed374c384764808e502dacfed6a67bcacd3aa555
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-02 16:08:21 +0000

    Add AES encryption utility for secure file handling

---
+++ core/bloombits/file_encryption.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "errors"
+    "io"
+    "os"
+)
+
+func encryptFile(inputPath, outputPath string, key []byte) error {
+    plaintext, err := os.ReadFile(inputPath)
+    if err != nil {
+        return err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+        return err
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+    return os.WriteFile(outputPath, ciphertext, 0644)
+}
+
+func decryptFile(inputPath string, key []byte) ([]byte, error) {
+    ciphertext, err := os.ReadFile(inputPath)
+    if err != nil {
+        return nil, err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(ciphertext) < nonceSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+    return gcm.Open(nil, nonce, ciphertext, nil)
+}
