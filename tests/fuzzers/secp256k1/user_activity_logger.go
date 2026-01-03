1cc21c9d02955c5d3c7a5e30a62d0ee28dd554ca
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-03 00:35:58 +0000

    Add user activity logging middleware with request details and timestamp

--- tests/fuzzers/secp256k1/user_activity_logger.go
+++ tests/fuzzers/secp256k1/user_activity_logger.go
@@ -16,108 +16,47 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	writer := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-	
-	al.handler.ServeHTTP(writer, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
-		r.Method,
-		r.URL.Path,
-		writer.statusCode,
-		duration,
-		r.UserAgent(),
-	)
-}
-
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	UserID    string
-	IPAddress string
-	Method    string
-	Path      string
-	Timestamp time.Time
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserID(authHeader)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			IPAddress: r.RemoteAddr,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			Timestamp: start,
-		}
-
-		logActivity(activity)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extractUserID(token string) string {
-	return "user_" + token[:8]
-}
-
-func logActivity(activity ActivityLog) {
-	log.Printf("ACTIVITY: User %s from %s %s %s at %v",
-		activity.UserID,
-		activity.IPAddress,
-		activity.Method,
-		activity.Path,
-		activity.Timestamp.Format(time.RFC3339),
-	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
 	
 	al.handler.ServeHTTP(w, r)
 	
 	duration := time.Since(start)
 	
-	log.Printf("Activity: %s %s from %s took %v",
+	log.Printf(
+		"Activity: %s %s | Status: %d | Duration: %v | UserAgent: %s | RemoteAddr: %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		getStatusCode(w),
 		duration,
+		r.UserAgent(),
+		r.RemoteAddr,
 	)
+}
+
+type responseWriterWrapper struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriterWrapper) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func getStatusCode(w http.ResponseWriter) int {
+	if rw, ok := w.(*responseWriterWrapper); ok {
+		return rw.statusCode
+	}
+	return http.StatusOK
+}
+
+func ActivityLoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrappedWriter := &responseWriterWrapper{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		logger := NewActivityLogger(next)
+		logger.ServeHTTP(wrappedWriter, r)
+	})
 }
