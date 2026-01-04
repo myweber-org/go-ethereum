98a079ab7a2f97f350a8a459f4654487561ff979
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-04 16:04:07 +0000

    Add configuration loader with YAML support and environment variable overrides

---
+++ cmd/evm/testdata/11/config_loader.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		URL      string `yaml:"url" env:"DB_URL"`
+		MaxConns int    `yaml:"max_connections" env:"DB_MAX_CONNS"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	overrideString(&cfg.Server.Host, "SERVER_HOST")
+	overrideInt(&cfg.Server.Port, "SERVER_PORT")
+	overrideString(&cfg.Database.URL, "DB_URL")
+	overrideInt(&cfg.Database.MaxConns, "DB_MAX_CONNS")
+	overrideString(&cfg.LogLevel, "LOG_LEVEL")
+}
+
+func overrideString(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
+	}
+}
+
+func overrideInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		var tmp int
+		if _, err := fmt.Sscanf(val, "%d", &tmp); err == nil {
+			*field = tmp
+		}
+	}
+}
