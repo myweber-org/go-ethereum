8197d1839ea97c7c8b8d6c04a22e565bdf5979e4
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-04 20:09:00 +0000

    Add user activity logging middleware for tracking API usage

---
+++ console/user_activity_logger.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration.String(),
+			r.RemoteAddr,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}
