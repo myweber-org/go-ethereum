6c4ddc873447f07f6bd5e4d72ad1096f20b66705
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-05 15:40:21 +0000

    Add configuration loader with environment variable support and validation

---
+++ internal/reexec/config_loader.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	ServerPort int
+	DBHost     string
+	DBPort     int
+	DebugMode  bool
+	APIKeys    []string
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+
+	port, err := strconv.Atoi(getEnv("SERVER_PORT", "8080"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.ServerPort = port
+
+	cfg.DBHost = getEnv("DB_HOST", "localhost")
+
+	dbPort, err := strconv.Atoi(getEnv("DB_PORT", "5432"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.DBPort = dbPort
+
+	debug, err := strconv.ParseBool(getEnv("DEBUG_MODE", "false"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.DebugMode = debug
+
+	keys := getEnv("API_KEYS", "")
+	if keys != "" {
+		cfg.APIKeys = strings.Split(keys, ",")
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
