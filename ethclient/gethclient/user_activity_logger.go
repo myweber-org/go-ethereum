b536bf2059196ef612fe93b042bd699f47917121
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 20:48:34 +0000

    Add user activity logging with timestamps and event types

--- ethclient/gethclient/user_activity_logger.go
+++ ethclient/gethclient/user_activity_logger.go
@@ -1,31 +1,70 @@
-
-package middleware
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
 	"time"
 )
 
+type ActivityEvent struct {
+	UserID    string    `json:"user_id"`
+	EventType string    `json:"event_type"`
+	Timestamp time.Time `json:"timestamp"`
+	Details   string    `json:"details"`
+}
+
 type ActivityLogger struct {
-	handler http.Handler
+	logFile *os.File
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+	event := ActivityEvent{
+		UserID:    userID,
+		EventType: eventType,
+		Timestamp: time.Now(),
+		Details:   details,
+	}
+
+	data, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = l.logFile.Write(data)
+	return err
+}
+
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	err = logger.LogActivity("user123", "login", "User logged in from web browser")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = logger.LogActivity("user123", "view_page", "Viewed dashboard page")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Activity logged successfully")
 }
