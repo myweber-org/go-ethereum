e35209a13cf6ca60ec70ee19d905015443c31d75
InkspillVerse <inkspill.verse@outlook.com>
2026-01-09 01:54:34 +0000

    Add AES encryption utility for secure data handling

--- tests/fuzzers/txfetcher/corpus/data_encryptor.go
+++ tests/fuzzers/txfetcher/corpus/data_encryptor.go
@@ -71,3 +71,64 @@ func main() {
 	}
 	fmt.Printf("Decrypted: %s\n", decrypted)
 }
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "io"
+)
+
+func encryptData(plaintext []byte, key []byte) (string, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return "", err
+    }
+
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+    return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decryptData(encrypted string, key []byte) ([]byte, error) {
+    ciphertext, err := base64.StdEncoding.DecodeString(encrypted)
+    if err != nil {
+        return nil, err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    stream := cipher.NewCFBDecrypter(block, iv)
+    stream.XORKeyStream(ciphertext, ciphertext)
+
+    return ciphertext, nil
+}
+
+func generateRandomKey() ([]byte, error) {
+    key := make([]byte, 32)
+    _, err := rand.Read(key)
+    if err != nil {
+        return nil, err
+    }
+    return key, nil
+}
