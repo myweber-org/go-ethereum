6db20cf97c7530927923c6e0c027b9263fca435e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 17:13:09 +0000

    Add user activity logging middleware for API endpoints

--- rpc/testdata/user_activity_logger.go
+++ rpc/testdata/user_activity_logger.go
@@ -72,4 +72,32 @@ func main() {
 	}
 
 	fmt.Println("Activity logged successfully")
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"User activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
