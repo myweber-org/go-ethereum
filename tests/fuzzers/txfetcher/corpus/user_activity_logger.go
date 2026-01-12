d5c5e3e1fa80d3da463557e1168d7bc536b76118
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-12 15:15:52 +0000

    Add user activity logging middleware for API endpoints

---
+++ tests/fuzzers/txfetcher/corpus/user_activity_logger.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userAgent := r.UserAgent()
+	ipAddress := r.RemoteAddr
+	requestPath := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("Activity: %s %s | IP: %s | Agent: %s | Duration: %v",
+		r.Method, requestPath, ipAddress, userAgent, duration)
+}
