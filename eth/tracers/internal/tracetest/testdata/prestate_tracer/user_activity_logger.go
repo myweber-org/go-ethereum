b2054335c1f3609a8b8b0f4c43539289d043ece3
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-13 23:59:19 +0000

    Add user activity logging middleware for tracking API requests

--- eth/tracers/internal/tracetest/testdata/prestate_tracer/user_activity_logger.go
+++ eth/tracers/internal/tracetest/testdata/prestate_tracer/user_activity_logger.go
@@ -1,3 +1,4 @@
+
 package middleware
 
 import (
@@ -16,11 +17,12 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+	
 	al.handler.ServeHTTP(w, r)
+	
 	duration := time.Since(start)
-
-	log.Printf(
-		"User activity: %s %s from %s took %v",
+	
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
 		r.RemoteAddr,
