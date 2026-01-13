7320efae047e33b0f25098129ddf5e00a938c2bd
InkspillVerse <inkspill.verse@outlook.com>
2026-01-13 19:04:23 +0000

    Add file encryption utility with AES-GCM and key derivation

---
+++ accounts/external/file_encryption_utility.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encrypt(plaintext []byte, passphrase string) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	result := make([]byte, len(salt)+len(ciphertext))
+	copy(result[:16], salt)
+	copy(result[16:], ciphertext)
+
+	return base64.StdEncoding.EncodeToString(result), nil
+}
+
+func decrypt(encodedCiphertext string, passphrase string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(encodedCiphertext)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < 16 {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	salt := data[:16]
+	ciphertext := data[16:]
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run file_encryption_utility.go <encrypt|decrypt> <input_file> <passphrase>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputFile := os.Args[2]
+	passphrase := os.Args[3]
+
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	switch operation {
+	case "encrypt":
+		encrypted, err := encrypt(data, passphrase)
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+		outputFile := inputFile + ".enc"
+		if err := os.WriteFile(outputFile, []byte(encrypted), 0644); err != nil {
+			fmt.Printf("Error writing file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Encrypted file saved as: %s\n", outputFile)
+
+	case "decrypt":
+		decrypted, err := decrypt(string(data), passphrase)
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+		outputFile := inputFile + ".dec"
+		if err := os.WriteFile(outputFile, decrypted, 0644); err != nil {
+			fmt.Printf("Error writing file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Decrypted file saved as: %s\n", outputFile)
+
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
