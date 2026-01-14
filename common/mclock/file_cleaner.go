22ccfa7294e34dda7bc199b93eacf129759d65e7
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-14 20:14:13 +0000

    Add utility to remove temporary files older than 7 days

---
+++ common/mclock/file_cleaner.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const retentionDays = 7
+
+func main() {
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			if err := os.RemoveAll(path); err != nil {
+				fmt.Printf("Failed to remove %s: %v\n", path, err)
+			} else {
+				fmt.Printf("Removed: %s\n", path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+	}
+}
