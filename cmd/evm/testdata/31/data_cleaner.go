4deac4d68dc29a4267e1e40736f58c427c3bc4ff
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-14 20:16:58 +0000

    Add data cleaning utility for CSV files with duplicate removal and trimming

--- cmd/evm/testdata/31/data_cleaner.go
+++ cmd/evm/testdata/31/data_cleaner.go
@@ -11,3 +11,68 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 	}
 	return result
 }
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+)
+
+func cleanCSV(inputPath, outputPath string) error {
+    inFile, err := os.Open(inputPath)
+    if err != nil {
+        return err
+    }
+    defer inFile.Close()
+
+    outFile, err := os.Create(outputPath)
+    if err != nil {
+        return err
+    }
+    defer outFile.Close()
+
+    reader := csv.NewReader(inFile)
+    writer := csv.NewWriter(outFile)
+    defer writer.Flush()
+
+    seen := make(map[string]bool)
+    for {
+        record, err := reader.Read()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            return err
+        }
+
+        for i, field := range record {
+            record[i] = strings.TrimSpace(field)
+        }
+
+        key := strings.Join(record, "|")
+        if !seen[key] {
+            seen[key] = true
+            if err := writer.Write(record); err != nil {
+                return err
+            }
+        }
+    }
+    return nil
+}
+
+func main() {
+    if len(os.Args) != 3 {
+        fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+        os.Exit(1)
+    }
+
+    err := cleanCSV(os.Args[1], os.Args[2])
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Println("CSV cleaning completed successfully")
+}
