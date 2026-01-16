abb548670ee126c7491e63e95997fbf957afaba3
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-16 01:23:02 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- eth/tracers/internal/data_cleaner.go
+++ eth/tracers/internal/data_cleaner.go
@@ -29,3 +29,66 @@ func CleanInputString(input string) string {
 func NormalizeWhitespace(text string) string {
 	return strings.Join(strings.Fields(text), " ")
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func removeDuplicates(inputFile, outputFile string) error {
+	inFile, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	reader := csv.NewReader(inFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		if len(record) > 0 {
+			key := record[0]
+			if !seen[key] {
+				seen[key] = true
+				uniqueRecords = append(uniqueRecords, record)
+			}
+		}
+	}
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	return writer.WriteAll(uniqueRecords)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	err := removeDuplicates(inputFile, outputFile)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully cleaned data. Output saved to %s\n", outputFile)
+}
