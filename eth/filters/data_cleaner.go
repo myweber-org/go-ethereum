f4e4ac7fbc19ded89a9fdbf70bdf7e79de7025af
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-17 16:32:50 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- eth/filters/data_cleaner.go
+++ eth/filters/data_cleaner.go
@@ -11,3 +11,26 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 	}
 	return result
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 6, 6, 7}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
