cc2cdbcde77734121b9288b90ebf3799b5c70170
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-17 21:33:27 +0000

    Add configuration loader with YAML support and validation

--- miner/config_loader.go
+++ miner/config_loader.go
@@ -1,53 +1,76 @@
 package config
 
 import (
-    "fmt"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"errors"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type DatabaseConfig struct {
-    Host     string `yaml:"host"`
-    Port     int    `yaml:"port"`
-    Username string `yaml:"username"`
-    Password string `yaml:"password"`
-    Name     string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+	SSLMode  string `yaml:"ssl_mode"`
 }
 
 type ServerConfig struct {
-    Port int    `yaml:"port"`
-    Mode string `yaml:"mode"`
+	Port         int    `yaml:"port"`
+	ReadTimeout  int    `yaml:"read_timeout"`
+	WriteTimeout int    `yaml:"write_timeout"`
+	IdleTimeout  int    `yaml:"idle_timeout"`
+	DebugMode    bool   `yaml:"debug_mode"`
+	LogLevel     string `yaml:"log_level"`
 }
 
 type AppConfig struct {
-    Database DatabaseConfig `yaml:"database"`
-    Server   ServerConfig   `yaml:"server"`
-    LogLevel string         `yaml:"log_level"`
+	Database DatabaseConfig `yaml:"database"`
+	Server   ServerConfig   `yaml:"server"`
+	Features []string       `yaml:"features"`
 }
 
-func LoadConfig(filePath string) (*AppConfig, error) {
-    data, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %w", err)
-    }
+func LoadConfig(path string) (*AppConfig, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var config AppConfig
-    if err := yaml.Unmarshal(data, &config); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %w", err)
-    }
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
 
-    return &config, nil
+	var config AppConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	if err := validateConfig(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
 
-func (c *AppConfig) Validate() error {
-    if c.Database.Host == "" {
-        return fmt.Errorf("database host is required")
-    }
-    if c.Database.Port <= 0 {
-        return fmt.Errorf("database port must be positive")
-    }
-    if c.Server.Port <= 0 {
-        return fmt.Errorf("server port must be positive")
-    }
-    return nil
+func validateConfig(config *AppConfig) error {
+	if config.Database.Host == "" {
+		return errors.New("database host is required")
+	}
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if config.Server.LogLevel == "" {
+		config.Server.LogLevel = "info"
+	}
+	if config.Database.SSLMode == "" {
+		config.Database.SSLMode = "disable"
+	}
+	return nil
 }
