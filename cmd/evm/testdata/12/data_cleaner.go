df166367fa4315d2748b6802f27a26bebe82767a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-17 21:48:16 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- cmd/evm/testdata/12/data_cleaner.go
+++ cmd/evm/testdata/12/data_cleaner.go
@@ -65,4 +65,17 @@ func main() {
 	validatedRecords := ValidateEmails(uniqueRecords)
 	fmt.Println("\nAfter validation:")
 	PrintRecords(validatedRecords)
+}package utils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
 }
