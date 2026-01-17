aa114b17d11f17238b89796a889a7eca3a1626d1
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-17 01:36:46 +0000

    Add file encryption utility with AES-256-GCM support

---
+++ eth/tracers/js/internal/tracers/file_encryption.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encrypt(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func generateKey() ([]byte, error) {
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func main() {
+	key, err := generateKey()
+	if err != nil {
+		fmt.Printf("Key generation failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	message := "Sensitive data requiring encryption"
+	ciphertext, err := encrypt([]byte(message), key)
+	if err != nil {
+		fmt.Printf("Encryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Encrypted: %s\n", base64.StdEncoding.EncodeToString(ciphertext))
+
+	plaintext, err := decrypt(ciphertext, key)
+	if err != nil {
+		fmt.Printf("Decryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Decrypted: %s\n", plaintext)
+}
