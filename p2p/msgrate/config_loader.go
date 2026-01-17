e27ec748f9d631eb7537f2b361f24dbb40d1b6d4
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-17 15:18:48 +0000

    Add configuration loader with environment variable support

--- p2p/msgrate/config_loader.go
+++ p2p/msgrate/config_loader.go
@@ -103,4 +103,70 @@ func validateConfig(config *Config) error {
 		return errors.New("invalid server port")
 	}
 	return nil
+}package config
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+)
+
+type Config struct {
+	ServerPort string `json:"server_port"`
+	DBHost     string `json:"db_host"`
+	DBPort     string `json:"db_port"`
+	LogLevel   string `json:"log_level"`
+}
+
+var (
+	instance *Config
+	once     sync.Once
+)
+
+func Load() *Config {
+	once.Do(func() {
+		instance = &Config{
+			ServerPort: getEnv("SERVER_PORT", "8080"),
+			DBHost:     getEnv("DB_HOST", "localhost"),
+			DBPort:     getEnv("DB_PORT", "5432"),
+			LogLevel:   getEnv("LOG_LEVEL", "info"),
+		}
+
+		if configFile := os.Getenv("CONFIG_FILE"); configFile != "" {
+			loadFromFile(configFile, instance)
+		}
+	})
+	return instance
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func loadFromFile(filename string, config *Config) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	var fileConfig Config
+	if err := json.Unmarshal(data, &fileConfig); err != nil {
+		return
+	}
+
+	if fileConfig.ServerPort != "" {
+		config.ServerPort = fileConfig.ServerPort
+	}
+	if fileConfig.DBHost != "" {
+		config.DBHost = fileConfig.DBHost
+	}
+	if fileConfig.DBPort != "" {
+		config.DBPort = fileConfig.DBPort
+	}
+	if fileConfig.LogLevel != "" {
+		config.LogLevel = fileConfig.LogLevel
+	}
 }
