b7898ca35ee3737abb2ebad206f5dd4e1959d2e9
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-18 16:12:01 +0000

    Add user activity logging middleware with request details and timestamp

--- beacon/light/api/user_activity_logger.go
+++ beacon/light/api/user_activity_logger.go
@@ -6,25 +6,31 @@ import (
 	"time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+		log.Printf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			duration,
+			r.RemoteAddr,
+		)
+	})
 }
