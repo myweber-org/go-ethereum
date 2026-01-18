42f54be0fe2f9270ae0d66a7d9b19256325b31a1
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-18 23:37:44 +0000

    Add user activity logging with timestamp and action type

--- ethclient/simulated/user_activity_logger.go
+++ ethclient/simulated/user_activity_logger.go
@@ -26,4 +26,78 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(start)
 	log.Printf("Activity: %s %s | IP: %s | Agent: %s | Duration: %v",
 		method, path, ipAddress, userAgent, duration)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type UserActivity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, action, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(activity)
+    if err != nil {
+        return err
+    }
+
+    data = append(data, '\n')
+    _, err = l.logFile.Write(data)
+    return err
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = logger.LogActivity("user123", "file_upload", "uploaded profile.jpg")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = logger.LogActivity("user456", "logout", "")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("User activities logged successfully")
 }
