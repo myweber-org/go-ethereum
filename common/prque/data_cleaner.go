841b866eb9145305c58ba2a383f70d497d80df2d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-19 00:53:02 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ common/prque/data_cleaner.go
@@ -0,0 +1,14 @@
+package datautils
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
