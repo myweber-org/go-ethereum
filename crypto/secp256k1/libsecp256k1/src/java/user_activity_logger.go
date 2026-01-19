aa1869f7187fb422e2350d671627d812319c9787
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-19 00:12:28 +0000

    Add user activity logging with timestamps and event types

--- crypto/secp256k1/libsecp256k1/src/java/user_activity_logger.go
+++ crypto/secp256k1/libsecp256k1/src/java/user_activity_logger.go
@@ -33,4 +33,72 @@ func ActivityLogger(next http.Handler) http.Handler {
 			duration,
 		)
 	})
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+    "time"
+)
+
+type ActivityEvent struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+    event := ActivityEvent{
+        Timestamp: time.Now(),
+        UserID:    userID,
+        EventType: eventType,
+        Details:   details,
+    }
+
+    data, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    _, err = l.logFile.Write(append(data, '\n'))
+    return err
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("activity.log")
+    if err != nil {
+        fmt.Printf("Failed to create logger: %v\n", err)
+        return
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "User logged in from web browser")
+    if err != nil {
+        fmt.Printf("Failed to log activity: %v\n", err)
+    }
+
+    err = logger.LogActivity("user123", "search", "Searched for 'golang tutorials'")
+    if err != nil {
+        fmt.Printf("Failed to log activity: %v\n", err)
+    }
+
+    fmt.Println("Activity logging completed")
 }
