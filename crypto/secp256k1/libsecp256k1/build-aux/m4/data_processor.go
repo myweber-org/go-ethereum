a7d1dd7d6f4fd41632dd2173b1d5054edd375adb
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-19 21:46:11 +0000

    Add data validation and sanitization functions for user input processing

---
+++ crypto/secp256k1/libsecp256k1/build-aux/m4/data_processor.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TruncateString(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen]
+}
