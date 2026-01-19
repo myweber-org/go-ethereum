f91731bca2be8d06d5a07adfd153df0994477deb
InkspillVerse <inkspill.verse@outlook.com>
2026-01-19 19:15:52 +0000

    Add configuration loader with YAML parsing support

---
+++ rlp/internal/config_loader.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Server.Port < 1 || c.Server.Port > 65535 {
+        return fmt.Errorf("invalid port number: %d", c.Server.Port)
+    }
+    if c.LogLevel == "" {
+        c.LogLevel = "info"
+    }
+    return nil
+}
