639cb42154538cc5a32982cc21c0deac5e7936af
InkspillVerse <inkspill.verse@outlook.com>
2026-01-20 19:53:44 +0000

    Add data processing utility for cleaning and normalizing input strings

--- p2p/simulations/data_processor.go
+++ p2p/simulations/data_processor.go
@@ -41,4 +41,54 @@ func main() {
 	pretty, _ := PrettyPrintJSON(sample)
 	fmt.Println("Pretty JSON:")
 	fmt.Println(pretty)
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	normalized := dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+	return normalized
+}
+
+func (dp *DataProcessor) NormalizeCase(input string, toUpper bool) string {
+	cleaned := dp.CleanString(input)
+	if toUpper {
+		return strings.ToUpper(cleaned)
+	}
+	return strings.ToLower(cleaned)
+}
+
+func (dp *DataProcessor) ExtractAlphanumeric(input string) string {
+	alnumRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	cleaned := dp.CleanString(input)
+	return alnumRegex.ReplaceAllString(cleaned, "")
+}
+
+func main() {
+	processor := NewDataProcessor()
+	
+	sample := "  Hello   World!  This  is  a  test.  "
+	
+	cleaned := processor.CleanString(sample)
+	println("Cleaned:", cleaned)
+	
+	upper := processor.NormalizeCase(sample, true)
+	println("Uppercase:", upper)
+	
+	alnum := processor.ExtractAlphanumeric(sample)
+	println("Alphanumeric only:", alnum)
 }
