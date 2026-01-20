6d92f317c562c2f74c1cc7ba8829eb88fd7848f6
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-20 23:28:13 +0000

    Add user activity logging with timestamp and action type

--- accounts/user_activity_logger.go
+++ accounts/user_activity_logger.go
@@ -1,45 +1,51 @@
-package middleware
+
+package main
 
 import (
-	"log"
-	"net/http"
-	"time"
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
+type UserActivity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func logActivity(userID, action, details string) {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.MarshalIndent(activity, "", "  ")
+    if err != nil {
+        log.Printf("Failed to marshal activity: %v", err)
+        return
+    }
+
+    fmt.Println(string(data))
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Printf("Failed to open log file: %v", err)
+        return
+    }
+    defer file.Close()
+
+    if _, err := file.Write(append(data, '\n')); err != nil {
+        log.Printf("Failed to write to log file: %v", err)
+    }
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-
-	al.handler.ServeHTTP(recorder, r)
-
-	duration := time.Since(start)
-	log.Printf(
-		"%s %s %d %s %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.RemoteAddr,
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+func main() {
+    logActivity("user123", "login", "Successful authentication")
+    logActivity("user456", "purchase", "Order ID: 78910")
+    logActivity("user123", "logout", "Session ended")
 }
