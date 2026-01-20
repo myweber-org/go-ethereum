88001f5a7c00d6b1e80aa76a3425e061b517f79e
InkspillVerse <inkspill.verse@outlook.com>
2026-01-20 22:55:24 +0000

    Add log rotation functionality with size-based file splitting

--- cmd/bootnode/log_rotator.go
+++ cmd/bootnode/log_rotator.go
@@ -130,3 +130,100 @@ func main() {
 		time.Sleep(100 * time.Millisecond)
 	}
 }
+package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "sync"
+    "time"
+)
+
+type RotatingWriter struct {
+    mu          sync.Mutex
+    current     *os.File
+    basePath    string
+    maxSize     int64
+    currentSize int64
+    maxFiles    int
+}
+
+func NewRotatingWriter(basePath string, maxSize int64, maxFiles int) (*RotatingWriter, error) {
+    w := &RotatingWriter{
+        basePath: basePath,
+        maxSize:  maxSize,
+        maxFiles: maxFiles,
+    }
+    if err := w.openCurrent(); err != nil {
+        return nil, err
+    }
+    return w, nil
+}
+
+func (w *RotatingWriter) openCurrent() error {
+    f, err := os.OpenFile(w.basePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+    if err != nil {
+        return err
+    }
+    stat, err := f.Stat()
+    if err != nil {
+        f.Close()
+        return err
+    }
+    w.current = f
+    w.currentSize = stat.Size()
+    return nil
+}
+
+func (w *RotatingWriter) rotate() error {
+    w.current.Close()
+    timestamp := time.Now().Unix()
+    for i := w.maxFiles - 1; i > 0; i-- {
+        oldPath := w.basePath + "." + strconv.Itoa(i-1)
+        newPath := w.basePath + "." + strconv.Itoa(i)
+        if _, err := os.Stat(oldPath); err == nil {
+            os.Rename(oldPath, newPath)
+        }
+    }
+    backupPath := w.basePath + ".0"
+    os.Rename(w.basePath, backupPath)
+    return w.openCurrent()
+}
+
+func (w *RotatingWriter) Write(p []byte) (int, error) {
+    w.mu.Lock()
+    defer w.mu.Unlock()
+    if w.currentSize+int64(len(p)) > w.maxSize {
+        if err := w.rotate(); err != nil {
+            return 0, err
+        }
+    }
+    n, err := w.current.Write(p)
+    if err == nil {
+        w.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (w *RotatingWriter) Close() error {
+    w.mu.Lock()
+    defer w.mu.Unlock()
+    return w.current.Close()
+}
+
+func main() {
+    writer, err := NewRotatingWriter("app.log", 1024*1024, 5)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to create writer: %v\n", err)
+        os.Exit(1)
+    }
+    defer writer.Close()
+    for i := 0; i < 100; i++ {
+        msg := fmt.Sprintf("Log entry %d at %s\n", i, time.Now().Format(time.RFC3339))
+        writer.Write([]byte(msg))
+        time.Sleep(100 * time.Millisecond)
+    }
+}
