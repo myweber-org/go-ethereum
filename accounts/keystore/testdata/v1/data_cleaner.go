51ef0742d650756267001d0a8b680c23962ac428
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-21 19:26:50 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- accounts/keystore/testdata/v1/data_cleaner.go
+++ accounts/keystore/testdata/v1/data_cleaner.go
@@ -21,3 +21,26 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Printf("Original: %v\n", data)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
