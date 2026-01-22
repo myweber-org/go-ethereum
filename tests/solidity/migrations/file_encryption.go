fb615be4aae13574a5aad6a30b83e3f97378224e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-22 00:35:20 +0000

    Add AES encryption utility for secure data handling

---
+++ tests/solidity/migrations/file_encryption.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "io"
+)
+
+func encryptData(plainText string, key []byte) (string, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return "", err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        return "", err
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, []byte(plainText), nil)
+    return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decryptData(encryptedText string, key []byte) (string, error) {
+    data, err := base64.StdEncoding.DecodeString(encryptedText)
+    if err != nil {
+        return "", err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return "", err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(data) < nonceSize {
+        return "", errors.New("ciphertext too short")
+    }
+
+    nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+    plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+    if err != nil {
+        return "", err
+    }
+
+    return string(plaintext), nil
+}
+
+func generateRandomKey() ([]byte, error) {
+    key := make([]byte, 32)
+    if _, err := rand.Read(key); err != nil {
+        return nil, err
+    }
+    return key, nil
+}
