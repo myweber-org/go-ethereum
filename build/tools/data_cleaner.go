ec044cdf4fe764e00a13d51cb70e5cf862c81e42
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-22 19:24:06 +0000

    Add utility function to sanitize user input by removing extra whitespace

---
+++ build/tools/data_cleaner.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeInput(input string) string {
+	// Trim leading and trailing whitespace
+	trimmed := strings.TrimSpace(input)
+	
+	// Replace multiple spaces with a single space
+	re := regexp.MustCompile(`\s+`)
+	cleaned := re.ReplaceAllString(trimmed, " ")
+	
+	return cleaned
+}
