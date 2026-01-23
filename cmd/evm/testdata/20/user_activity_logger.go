7789b350c930e40685c808899ee07845f9205e5b
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-23 19:14:06 +0000

    Add user activity logging middleware for API endpoints

--- cmd/evm/testdata/20/user_activity_logger.go
+++ cmd/evm/testdata/20/user_activity_logger.go
@@ -47,4 +47,32 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+		r.Method,
+		r.URL.Path,
+		duration,
+		time.Now().Format(time.RFC3339),
+	)
 }
