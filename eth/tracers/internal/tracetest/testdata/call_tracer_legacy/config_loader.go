cb79eeedb6cf910e792b122a1dce4c5bb999ded2
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-23 16:22:34 +0000

    Add configuration loader with environment variable support

--- eth/tracers/internal/tracetest/testdata/call_tracer_legacy/config_loader.go
+++ eth/tracers/internal/tracetest/testdata/call_tracer_legacy/config_loader.go
@@ -1,64 +1,43 @@
-
 package config
 
 import (
-	"errors"
-	"io"
-	"os"
-
-	"gopkg.in/yaml.v3"
+    "os"
+    "strconv"
+    "strings"
 )
 
 type Config struct {
-	Server struct {
-		Host string `yaml:"host"`
-		Port int    `yaml:"port"`
-	} `yaml:"server"`
-	Database struct {
-		Host     string `yaml:"host"`
-		Username string `yaml:"username"`
-		Password string `yaml:"password"`
-		Name     string `yaml:"name"`
-	} `yaml:"database"`
-	LogLevel string `yaml:"log_level"`
+    ServerPort int
+    DebugMode  bool
+    DatabaseURL string
+    AllowedHosts []string
 }
 
-func LoadConfig(path string) (*Config, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var config Config
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		return nil, err
-	}
-
-	if err := validateConfig(&config); err != nil {
-		return nil, err
-	}
-
-	return &config, nil
+func Load() (*Config, error) {
+    cfg := &Config{}
+    
+    portStr := getEnv("SERVER_PORT", "8080")
+    port, err := strconv.Atoi(portStr)
+    if err != nil {
+        return nil, err
+    }
+    cfg.ServerPort = port
+    
+    debugStr := getEnv("DEBUG_MODE", "false")
+    cfg.DebugMode = strings.ToLower(debugStr) == "true"
+    
+    cfg.DatabaseURL = getEnv("DATABASE_URL", "postgres://localhost:5432/app")
+    
+    hostsStr := getEnv("ALLOWED_HOSTS", "localhost,127.0.0.1")
+    cfg.AllowedHosts = strings.Split(hostsStr, ",")
+    
+    return cfg, nil
 }
 
-func validateConfig(c *Config) error {
-	if c.Server.Host == "" {
-		return errors.New("server host cannot be empty")
-	}
-	if c.Server.Port <= 0 || c.Server.Port > 65535 {
-		return errors.New("server port must be between 1 and 65535")
-	}
-	if c.Database.Host == "" {
-		return errors.New("database host cannot be empty")
-	}
-	if c.LogLevel == "" {
-		c.LogLevel = "info"
-	}
-	return nil
+func getEnv(key, defaultValue string) string {
+    value := os.Getenv(key)
+    if value == "" {
+        return defaultValue
+    }
+    return value
 }
