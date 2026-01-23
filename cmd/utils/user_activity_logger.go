25bacf1fe1d42cbbec557cd0ac8e882926876829
InkspillVerse <inkspill.verse@outlook.com>
2026-01-23 18:52:37 +0000

    Add user activity logging middleware for tracking API usage

--- cmd/utils/user_activity_logger.go
+++ cmd/utils/user_activity_logger.go
@@ -46,4 +46,33 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(startTime)
+	
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
