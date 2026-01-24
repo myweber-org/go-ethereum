5f57346e32113b608ca9474ced952b0da7ebac3e
InkspillVerse <inkspill.verse@outlook.com>
2026-01-24 23:01:57 +0000

    Add utility to remove temporary files older than 7 days

---
+++ cmd/evm/testdata/27/file_cleaner.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -7)
+
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing old file: %s\n", path)
+			os.RemoveAll(path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error cleaning temp directory: %v\n", err)
+	}
+}
