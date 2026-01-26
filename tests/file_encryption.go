418be8fd43705de2cc908680c448d2aab4427206
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-26 00:52:20 +0000

    Add AES encryption utility for secure data handling

---
+++ tests/file_encryption.go
@@ -0,0 +1,91 @@
+
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func encryptData(plainText string, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, []byte(plainText), nil)
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func decryptData(encryptedText string, key []byte) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, cipherText := data[:nonceSize], data[nonceSize:]
+
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainText), nil
+}
+
+func generateRandomKey() ([]byte, error) {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func main() {
+	key, err := generateRandomKey()
+	if err != nil {
+		fmt.Println("Error generating key:", err)
+		return
+	}
+
+	originalText := "Sensitive information requiring protection"
+	fmt.Println("Original text:", originalText)
+
+	encrypted, err := encryptData(originalText, key)
+	if err != nil {
+		fmt.Println("Encryption error:", err)
+		return
+	}
+	fmt.Println("Encrypted text:", encrypted)
+
+	decrypted, err := decryptData(encrypted, key)
+	if err != nil {
+		fmt.Println("Decryption error:", err)
+		return
+	}
+	fmt.Println("Decrypted text:", decrypted)
+}
