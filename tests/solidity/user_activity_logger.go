77a97e67334e7847aa6319f0042dc23402fbcebb
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-26 23:16:20 +0000

    Add user activity logging middleware for API endpoints

--- tests/solidity/user_activity_logger.go
+++ tests/solidity/user_activity_logger.go
@@ -27,4 +27,48 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(startTime)
+	log.Printf(
+		"[%s] %s %s %d %v",
+		time.Now().Format(time.RFC3339),
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
