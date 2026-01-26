037f006456f71ff546f8ec0c66b6343a904b5da4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-26 19:41:03 +0000

    Add configuration loader with YAML parsing support

--- eth/gasprice/config_loader.go
+++ eth/gasprice/config_loader.go
@@ -43,4 +43,39 @@ func (c *Config) Validate() error {
         return fmt.Errorf("database name cannot be empty")
     }
     return nil
+}package config
+
+import (
+    "io/ioutil"
+    "log"
+
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    var config Config
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        log.Printf("Failed to parse YAML: %v", err)
+        return nil, err
+    }
+
+    return &config, nil
 }
