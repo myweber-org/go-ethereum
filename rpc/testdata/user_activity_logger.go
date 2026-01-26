40c1fa8c35e9754791d7cbd61f24d54246b964ca
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-26 15:41:44 +0000

    Add user activity logging middleware for tracking API usage

--- rpc/testdata/user_activity_logger.go
+++ rpc/testdata/user_activity_logger.go
@@ -1,78 +1,4 @@
-package main
-
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-	"time"
-)
-
-type ActivityEvent struct {
-	Timestamp time.Time `json:"timestamp"`
-	UserID    string    `json:"user_id"`
-	EventType string    `json:"event_type"`
-	Details   string    `json:"details"`
-}
-
-type ActivityLogger struct {
-	logFile *os.File
-}
-
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return &ActivityLogger{logFile: file}, nil
-}
-
-func (al *ActivityLogger) LogActivity(userID, eventType, details string) error {
-	event := ActivityEvent{
-		Timestamp: time.Now(),
-		UserID:    userID,
-		EventType: eventType,
-		Details:   details,
-	}
-
-	eventJSON, err := json.Marshal(event)
-	if err != nil {
-		return err
-	}
-
-	eventJSON = append(eventJSON, '\n')
-	_, err = al.logFile.Write(eventJSON)
-	return err
-}
-
-func (al *ActivityLogger) Close() error {
-	return al.logFile.Close()
-}
-
-func main() {
-	logger, err := NewActivityLogger("activity.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logger.Close()
-
-	err = logger.LogActivity("user123", "LOGIN", "User logged in from web browser")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = logger.LogActivity("user123", "VIEW_PAGE", "Viewed dashboard page")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = logger.LogActivity("user456", "UPLOAD", "Uploaded profile picture")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Activity logged successfully")
-}package middleware
+package middleware
 
 import (
 	"log"
@@ -93,8 +19,7 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
 
-	log.Printf(
-		"User activity: %s %s from %s took %v",
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
 		r.RemoteAddr,
