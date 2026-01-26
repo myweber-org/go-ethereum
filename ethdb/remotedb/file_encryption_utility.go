b4de88894b407ca17a94599b6b880fb05b74659c
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-26 16:44:49 +0000

    Add file encryption utility with AES-256-GCM and secure key derivation

---
+++ ethdb/remotedb/file_encryption_utility.go
@@ -0,0 +1,146 @@
+
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	saltSize   = 16
+	nonceSize  = 12
+	keyLen     = 32
+	iterations = 100000
+)
+
+func deriveKey(password, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write(password)
+	hash.Write(salt)
+	for i := 0; i < iterations-1; i++ {
+		hash.Write(hash.Sum(nil))
+	}
+	return hash.Sum(nil)
+}
+
+func encryptData(plaintext, password []byte) ([]byte, error) {
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return nil, err
+	}
+
+	key := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, nonceSize)
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	result := make([]byte, 0, len(salt)+len(nonce)+len(ciphertext))
+	result = append(result, salt...)
+	result = append(result, nonce...)
+	result = append(result, ciphertext...)
+
+	return result, nil
+}
+
+func decryptData(ciphertext, password []byte) ([]byte, error) {
+	if len(ciphertext) < saltSize+nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	salt := ciphertext[:saltSize]
+	nonce := ciphertext[saltSize : saltSize+nonceSize]
+	actualCiphertext := ciphertext[saltSize+nonceSize:]
+
+	key := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, actualCiphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run file_encryption_utility.go <encrypt|decrypt> <input_file> <output_file>")
+		fmt.Println("Password will be read from environment variable ENCRYPTION_PASSWORD")
+		return
+	}
+
+	operation := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	password := os.Getenv("ENCRYPTION_PASSWORD")
+	if password == "" {
+		fmt.Println("Error: ENCRYPTION_PASSWORD environment variable not set")
+		os.Exit(1)
+	}
+
+	inputData, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading input file: %v\n", err)
+		os.Exit(1)
+	}
+
+	var outputData []byte
+	switch operation {
+	case "encrypt":
+		outputData, err = encryptData(inputData, []byte(password))
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Encrypted %d bytes to %d bytes\n", len(inputData), len(outputData))
+
+	case "decrypt":
+		outputData, err = decryptData(inputData, []byte(password))
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Decrypted %d bytes to %d bytes\n", len(inputData), len(outputData))
+
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+
+	if err := os.WriteFile(outputFile, outputData, 0644); err != nil {
+		fmt.Printf("Error writing output file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Operation completed successfully. Output written to %s\n", outputFile)
+}
