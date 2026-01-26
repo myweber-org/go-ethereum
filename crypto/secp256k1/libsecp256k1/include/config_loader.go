8868ad0264588a1da10440299db3156d606770ee
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-26 21:01:16 +0000

    Add configuration loader with YAML parsing support

--- crypto/secp256k1/libsecp256k1/include/config_loader.go
+++ crypto/secp256k1/libsecp256k1/include/config_loader.go
@@ -280,4 +280,60 @@ func (c *Config) String() string {
 	displayConfig.Database.Password = "***"
 	data, _ := json.MarshalIndent(displayConfig, "", "  ")
 	return string(data)
+}package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
+
+type ServerConfig struct {
+	Port int    `yaml:"port"`
+	Mode string `yaml:"mode"`
+}
+
+type AppConfig struct {
+	Database DatabaseConfig `yaml:"database"`
+	Server   ServerConfig   `yaml:"server"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+	if config.Database.Host == "" {
+		log.Println("Database host is required")
+		return false
+	}
+	if config.Database.Port <= 0 {
+		log.Println("Database port must be positive")
+		return false
+	}
+	if config.Server.Port <= 0 {
+		log.Println("Server port must be positive")
+		return false
+	}
+	return true
 }
