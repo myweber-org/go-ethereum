9f1898399accb44d89271c9f7bc5f6f26e9508b5
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-27 19:10:12 +0000

    Add configuration loader with YAML parsing support

---
+++ crypto/secp256k1/libsecp256k1/sage/config_loader.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    if config.Server.Host == "" {
+        config.Server.Host = "localhost"
+    }
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+    if config.LogLevel == "" {
+        config.LogLevel = "info"
+    }
+
+    return &config, nil
+}
