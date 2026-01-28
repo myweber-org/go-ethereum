8e4ec4ca359cdd9c554e4dcb441d71abc947eff4
InkspillVerse <inkspill.verse@outlook.com>
2026-01-28 22:18:25 +0000

    Add data processing function to calculate average from slice of integers

--- eth/protocols/data_processor.go
+++ eth/protocols/data_processor.go
@@ -137,4 +137,23 @@ func (dp *DataProcessor) ProcessUserData(name, email string) (string, bool) {
 	}
 
 	return sanitizedName + ":" + sanitizedEmail, true
+}package main
+
+import "fmt"
+
+func calculateAverage(numbers []int) float64 {
+    if len(numbers) == 0 {
+        return 0
+    }
+    sum := 0
+    for _, num := range numbers {
+        sum += num
+    }
+    return float64(sum) / float64(len(numbers))
+}
+
+func main() {
+    data := []int{10, 20, 30, 40, 50}
+    avg := calculateAverage(data)
+    fmt.Printf("Average: %.2f\n", avg)
 }
