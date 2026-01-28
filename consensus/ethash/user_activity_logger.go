ba3b11e4a953f92c7efd6cbb757f4198e314758b
InkspillVerse <inkspill.verse@outlook.com>
2026-01-28 22:45:17 +0000

    Add user activity logging middleware with request details and timestamp

--- consensus/ethash/user_activity_logger.go
+++ consensus/ethash/user_activity_logger.go
@@ -7,44 +7,24 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
+	handler http.Handler
 }
 
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+	log.Printf(
+		"[%s] %s %s %s %v",
+		time.Now().Format(time.RFC3339),
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
