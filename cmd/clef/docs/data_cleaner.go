4c39d76fe577497c8c84597cfade6fa4e7d97ff5
InkspillVerse <inkspill.verse@outlook.com>
2026-01-28 16:16:32 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- cmd/clef/docs/data_cleaner.go
+++ cmd/clef/docs/data_cleaner.go
@@ -20,4 +20,44 @@ func CleanInput(input string) string {
 func ValidateEmail(email string) bool {
 	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 	return emailRegex.MatchString(email)
+}package datautil
+
+import "sort"
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	seen := make(map[T]bool)
+	result := make([]T, 0, len(slice))
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func RemoveDuplicatesSorted[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		// Convert to string for comparable types for sorting
+		return fmt.Sprintf("%v", slice[i]) < fmt.Sprintf("%v", slice[j])
+	})
+
+	result := slice[:1]
+	for i := 1; i < len(slice); i++ {
+		if slice[i] != slice[i-1] {
+			result = append(result, slice[i])
+		}
+	}
+
+	return result
 }
