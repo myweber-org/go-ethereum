e53bcbb9ab3ebf91cf6c6339363c281856a643e5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-28 01:05:05 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- docs/data_cleaner.go
+++ docs/data_cleaner.go
@@ -1,83 +1,24 @@
 
 package main
 
-import (
-	"encoding/csv"
-	"fmt"
-	"io"
-	"os"
-)
-
-func removeDuplicates(inputFile, outputFile string) error {
-	in, err := os.Open(inputFile)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	reader := csv.NewReader(in)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return err
-	}
-
-	seen := make(map[string]bool)
-	var uniqueRecords [][]string
-
-	for _, record := range records {
-		if len(record) == 0 {
-			continue
-		}
-		key := record[0]
-		if !seen[key] {
-			seen[key] = true
-			uniqueRecords = append(uniqueRecords, record)
-		}
-	}
-
-	out, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	writer := csv.NewWriter(out)
-	return writer.WriteAll(uniqueRecords)
-}
-
-func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
-		os.Exit(1)
-	}
-
-	err := removeDuplicates(os.Args[1], os.Args[2])
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Duplicate removal completed successfully")
-}package main
-
 import "fmt"
 
-func RemoveDuplicates(input []int) []int {
+func RemoveDuplicates(nums []int) []int {
 	seen := make(map[int]bool)
 	result := []int{}
 
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
 		}
 	}
 	return result
 }
 
 func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5}
-	cleaned := RemoveDuplicates(data)
-	fmt.Println("Original:", data)
+	input := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(input)
+	fmt.Println("Original:", input)
 	fmt.Println("Cleaned:", cleaned)
 }
