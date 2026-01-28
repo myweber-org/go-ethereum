b0a6401002727a6a412c793960cdc261aee03e20
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 21:39:18 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- eth/tracers/internal/tracetest/data_cleaner.go
+++ eth/tracers/internal/tracetest/data_cleaner.go
@@ -44,4 +44,26 @@ func NormalizeWhitespace(input string) string {
 	}
 	
 	return result.String()
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 2, 3, 4, 4, 5, 5, 1}
+	cleaned := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
+	fmt.Printf("Cleaned: %v\n", cleaned)
 }
