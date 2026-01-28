38c7032e3606f0061d85d3d8f059b60b7bd76950
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-28 20:31:53 +0000

    Add configuration loader with YAML parsing support

--- graphql/config_loader.go
+++ graphql/config_loader.go
@@ -64,4 +64,54 @@ func getEnvBool(key string, defaultValue bool) (bool, error) {
 		return strconv.ParseBool(value)
 	}
 	return defaultValue, nil
+}package config
+
+import (
+    "io/ioutil"
+    "log"
+
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Name     string `yaml:"name"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(filename string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    var config AppConfig
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, err
+    }
+
+    return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+    if config.Server.Port <= 0 || config.Server.Port > 65535 {
+        log.Printf("Invalid server port: %d", config.Server.Port)
+        return false
+    }
+    
+    if config.Database.Host == "" {
+        log.Print("Database host cannot be empty")
+        return false
+    }
+    
+    return true
 }
