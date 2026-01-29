063824b55ab81dc0379808d8e61091e0ff0f8aa9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-29 19:32:25 +0000

    Add configuration loader with YAML parsing and validation support

---
+++ common/hexutil/config_loader.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+    "errors"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Name     string `yaml:"name"`
+        User     string `yaml:"user"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, err
+    }
+
+    var config Config
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, err
+    }
+
+    if config.Server.Host == "" {
+        config.Server.Host = "localhost"
+    }
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+
+    if config.Database.Host == "" || config.Database.Name == "" {
+        return nil, errors.New("database configuration is incomplete")
+    }
+
+    return &config, nil
+}
