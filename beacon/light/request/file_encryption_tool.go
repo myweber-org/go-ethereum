6c0b44a16e26b28562b3cac3f90d3beb18271c93
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-29 20:50:17 +0000

    Add file encryption utility with AES-256-GCM and key derivation

---
+++ beacon/light/request/file_encryption_tool.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+func encryptData(plaintext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func main() {
+	secretMessage := "Sensitive data requiring protection"
+	password := "securePass123!"
+
+	encrypted, err := encryptData([]byte(secretMessage), password)
+	if err != nil {
+		fmt.Printf("Encryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Encrypted (hex): %s\n", hex.EncodeToString(encrypted))
+
+	decrypted, err := decryptData(encrypted, password)
+	if err != nil {
+		fmt.Printf("Decryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Decrypted: %s\n", string(decrypted))
+}
