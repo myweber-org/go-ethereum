73981abfbfdd24297500dd9da3f01c7ecfacfa94
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-30 15:32:00 +0000

    Add data cleaning utility for CSV files

---
+++ rpc/data_cleaner.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func cleanCSV(inputPath, outputPath string) error {
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer outFile.Close()
+
+	reader := csv.NewReader(inFile)
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	headerProcessed := false
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read CSV record: %w", err)
+		}
+
+		if !headerProcessed {
+			headerProcessed = true
+			if err := writer.Write(record); err != nil {
+				return fmt.Errorf("failed to write header: %w", err)
+			}
+			continue
+		}
+
+		cleanedRecord := make([]string, len(record))
+		for i, field := range record {
+			cleanedField := strings.TrimSpace(field)
+			cleanedField = strings.ToUpper(cleanedField)
+			cleanedRecord[i] = cleanedField
+		}
+
+		if err := writer.Write(cleanedRecord); err != nil {
+			return fmt.Errorf("failed to write cleaned record: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	if err := cleanCSV(inputFile, outputFile); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully cleaned data from %s to %s\n", inputFile, outputFile)
+}
