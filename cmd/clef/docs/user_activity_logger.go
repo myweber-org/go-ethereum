cc5685dcd4e57439ba961cc1cb54f8bf086c3c79
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-30 15:53:11 +0000

    Add user activity logging middleware for tracking API usage

--- cmd/clef/docs/user_activity_logger.go
+++ cmd/clef/docs/user_activity_logger.go
@@ -1,76 +1,49 @@
-package main
+package middleware
 
 import (
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type EventType string
-
-const (
-	Login    EventType = "LOGIN"
-	Logout   EventType = "LOGOUT"
-	Purchase EventType = "PURCHASE"
-	View     EventType = "VIEW"
-)
-
-type UserActivity struct {
-	UserID    string
-	Event     EventType
-	Timestamp time.Time
-	Metadata  map[string]string
+type ActivityLogger struct {
+	Logger *log.Logger
 }
 
-func NewUserActivity(userID string, event EventType) *UserActivity {
-	return &UserActivity{
-		UserID:    userID,
-		Event:     event,
-		Timestamp: time.Now().UTC(),
-		Metadata:  make(map[string]string),
-	}
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
 }
 
-func (ua *UserActivity) AddMetadata(key, value string) {
-	ua.Metadata[key] = value
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+			r.UserAgent(),
+		)
+	})
 }
 
-func (ua *UserActivity) Log() {
-	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Printf("Failed to open log file: %v", err)
-		return
-	}
-	defer file.Close()
-
-	logEntry := fmt.Sprintf("[%s] User: %s, Event: %s",
-		ua.Timestamp.Format(time.RFC3339),
-		ua.UserID,
-		ua.Event,
-	)
-
-	if len(ua.Metadata) > 0 {
-		logEntry += fmt.Sprintf(", Metadata: %v", ua.Metadata)
-	}
-
-	logEntry += "\n"
-
-	if _, err := file.WriteString(logEntry); err != nil {
-		log.Printf("Failed to write log entry: %v", err)
-	}
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func main() {
-	activity := NewUserActivity("user123", Login)
-	activity.AddMetadata("ip", "192.168.1.100")
-	activity.AddMetadata("user_agent", "Mozilla/5.0")
-	activity.Log()
-
-	purchaseActivity := NewUserActivity("user456", Purchase)
-	purchaseActivity.AddMetadata("item_id", "prod_789")
-	purchaseActivity.AddMetadata("amount", "49.99")
-	purchaseActivity.Log()
-
-	fmt.Println("Activity logging completed")
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
