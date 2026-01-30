aaaab06a065dc8f2470bbdc395d36f9ce6c9c091
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-30 14:31:37 +0000

    Implement a basic log rotation mechanism with file size limit and backup file creation

--- graphql/log_rotator.go
+++ graphql/log_rotator.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -7,112 +6,88 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
-	"time"
 )
 
-type RotatingLogger struct {
-	mu           sync.Mutex
-	currentFile  *os.File
-	filePath     string
-	maxSize      int64
-	currentSize  int64
-	rotationCount int
+const maxFileSize = 1024 * 1024 // 1 MB
+const backupSuffix = ".bak"
+
+type LogRotator struct {
+	filePath string
+	file     *os.File
+	mu       sync.Mutex
 }
 
-func NewRotatingLogger(filePath string, maxSize int64) (*RotatingLogger, error) {
-	rl := &RotatingLogger{
-		filePath: filePath,
-		maxSize:  maxSize,
-	}
-
-	if err := rl.openCurrentFile(); err != nil {
-		return nil, err
-	}
-
-	return rl, nil
+func NewLogRotator(filePath string) (*LogRotator, error) {
+	lr := &LogRotator{filePath: filePath}
+	err := lr.openFile()
+	return lr, err
 }
 
-func (rl *RotatingLogger) openCurrentFile() error {
-	dir := filepath.Dir(rl.filePath)
+func (lr *LogRotator) openFile() error {
+	dir := filepath.Dir(lr.filePath)
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return err
 	}
 
-	file, err := os.OpenFile(rl.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(lr.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
-
-	info, err := file.Stat()
-	if err != nil {
-		file.Close()
-		return err
-	}
-
-	rl.currentFile = file
-	rl.currentSize = info.Size()
+	lr.file = file
 	return nil
 }
 
-func (rl *RotatingLogger) rotate() error {
-	if rl.currentFile != nil {
-		rl.currentFile.Close()
+func (lr *LogRotator) Write(p []byte) (int, error) {
+	lr.mu.Lock()
+	defer lr.mu.Unlock()
+
+	info, err := lr.file.Stat()
+	if err != nil {
+		return 0, err
 	}
 
-	timestamp := time.Now().Format("20060102_150405")
-	backupPath := fmt.Sprintf("%s.%s.%d", rl.filePath, timestamp, rl.rotationCount)
-	rl.rotationCount++
-
-	if err := os.Rename(rl.filePath, backupPath); err != nil {
-		return err
-	}
-
-	return rl.openCurrentFile()
-}
-
-func (rl *RotatingLogger) Write(p []byte) (int, error) {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
-
-	if rl.currentSize+int64(len(p)) > rl.maxSize {
-		if err := rl.rotate(); err != nil {
+	if info.Size()+int64(len(p)) > maxFileSize {
+		if err := lr.rotate(); err != nil {
 			return 0, err
 		}
 	}
 
-	n, err := rl.currentFile.Write(p)
-	if err == nil {
-		rl.currentSize += int64(n)
-	}
-	return n, err
+	return lr.file.Write(p)
 }
 
-func (rl *RotatingLogger) Close() error {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
-
-	if rl.currentFile != nil {
-		return rl.currentFile.Close()
+func (lr *LogRotator) rotate() error {
+	if err := lr.file.Close(); err != nil {
+		return err
 	}
-	return nil
+
+	backupPath := lr.filePath + backupSuffix
+	if err := os.Rename(lr.filePath, backupPath); err != nil {
+		return err
+	}
+
+	return lr.openFile()
+}
+
+func (lr *LogRotator) Close() error {
+	lr.mu.Lock()
+	defer lr.mu.Unlock()
+	return lr.file.Close()
 }
 
 func main() {
-	logger, err := NewRotatingLogger("logs/app.log", 1024*1024) // 1MB max size
+	rotator, err := NewLogRotator("logs/app.log")
 	if err != nil {
-		fmt.Printf("Failed to create logger: %v\n", err)
+		fmt.Printf("Failed to create log rotator: %v\n", err)
 		return
 	}
-	defer logger.Close()
+	defer rotator.Close()
 
-	for i := 0; i < 1000; i++ {
-		message := fmt.Sprintf("[%s] Log entry %d: Application is running normally\n", 
-			time.Now().Format(time.RFC3339), i)
-		if _, err := logger.Write([]byte(message)); err != nil {
+	for i := 0; i < 100; i++ {
+		message := fmt.Sprintf("Log entry number %d\n", i)
+		if _, err := rotator.Write([]byte(message)); err != nil {
 			fmt.Printf("Write error: %v\n", err)
 			break
 		}
-		time.Sleep(10 * time.Millisecond)
 	}
 
 	fmt.Println("Log rotation test completed")
