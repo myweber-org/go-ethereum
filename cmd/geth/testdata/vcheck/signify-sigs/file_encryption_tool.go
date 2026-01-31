938f61c40e22c48228886a7997a6eb1de76c4f20
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-31 23:52:03 +0000

    Add AES-256 file encryption utility with secure key derivation

---
+++ cmd/geth/testdata/vcheck/signify-sigs/file_encryption_tool.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const (
+	saltSize      = 16
+	nonceSize     = 12
+	keyIterations = 100000
+)
+
+func deriveKey(password string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hash.Write(salt)
+	for i := 0; i < keyIterations; i++ {
+		hash.Write(hash.Sum(nil))
+	}
+	return hash.Sum(nil)[:32]
+}
+
+func encryptFile(inputPath, outputPath, password string) error {
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey(password, salt)
+
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, nonceSize)
+	if _, err := rand.Read(nonce); err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	outputData := append(salt, nonce...)
+	outputData = append(outputData, ciphertext...)
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func decryptFile(inputPath, outputPath, password string) error {
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(data) < saltSize+nonceSize {
+		return errors.New("file too short")
+	}
+
+	salt := data[:saltSize]
+	nonce := data[saltSize : saltSize+nonceSize]
+	ciphertext := data[saltSize+nonceSize:]
+
+	key := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return errors.New("decryption failed: invalid password or corrupted file")
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Printf("Usage: %s <encrypt|decrypt> <input> <output> <password>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	password := os.Args[4]
+
+	var err error
+	switch operation {
+	case "encrypt":
+		err = encryptFile(inputPath, outputPath, password)
+	case "decrypt":
+		err = decryptFile(inputPath, outputPath, password)
+	default:
+		err = errors.New("invalid operation. Use 'encrypt' or 'decrypt'")
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Operation completed successfully: %s -> %s\n", inputPath, outputPath)
+}
