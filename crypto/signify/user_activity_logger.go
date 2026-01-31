87b9a43302deb8b41e3de1212ef528bee9739700
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-31 00:55:25 +0000

    Add user activity logging middleware for tracking API usage

---
+++ crypto/signify/user_activity_logger.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"[%s] %s %s %d %s",
+			time.Now().Format(time.RFC3339),
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}
