401bd70ef40df6538471359efd24525b11cfa8a5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-31 20:06:36 +0000

    Add user activity logging middleware for HTTP requests

--- accounts/keystore/user_activity_logger.go
+++ accounts/keystore/user_activity_logger.go
@@ -7,86 +7,23 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-    "log"
-    "net/http"
-    "time"
-)
-
-type ActivityLogger struct {
-    handler http.Handler
+	handler http.Handler
 }
 
 func NewActivityLogger(handler http.Handler) *ActivityLogger {
-    return &ActivityLogger{handler: handler}
+	return &ActivityLogger{handler: handler}
 }
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
-    
-    rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-    al.handler.ServeHTTP(rw, r)
-    
-    duration := time.Since(start)
-    
-    log.Printf(
-        "%s %s %d %s %s",
-        r.Method,
-        r.URL.Path,
-        rw.statusCode,
-        duration,
-        r.RemoteAddr,
-    )
-}
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-type responseWriter struct {
-    http.ResponseWriter
-    statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-    rw.statusCode = code
-    rw.ResponseWriter.WriteHeader(code)
+	log.Printf(
+		"Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
