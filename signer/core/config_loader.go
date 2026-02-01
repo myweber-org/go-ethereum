77ad0950baf9b337e79a5782ee5c3840a58d67ef
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-01 14:40:28 +0000

    Add configuration loader with environment variable support

--- signer/core/config_loader.go
+++ signer/core/config_loader.go
@@ -40,4 +40,71 @@ func getEnvWithDefault(key, defaultValue string) string {
         return defaultValue
     }
     return value
+}package config
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+)
+
+type Config struct {
+	ServerPort string `json:"server_port"`
+	DBHost     string `json:"db_host"`
+	DBPort     string `json:"db_port"`
+	LogLevel   string `json:"log_level"`
+}
+
+var (
+	instance *Config
+	once     sync.Once
+)
+
+func Load() *Config {
+	once.Do(func() {
+		instance = &Config{
+			ServerPort: getEnv("SERVER_PORT", "8080"),
+			DBHost:     getEnv("DB_HOST", "localhost"),
+			DBPort:     getEnv("DB_PORT", "5432"),
+			LogLevel:   getEnv("LOG_LEVEL", "info"),
+		}
+
+		configFile := os.Getenv("CONFIG_FILE")
+		if configFile != "" {
+			loadFromFile(configFile)
+		}
+	})
+	return instance
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func loadFromFile(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	var fileConfig Config
+	if err := json.Unmarshal(data, &fileConfig); err != nil {
+		return
+	}
+
+	if fileConfig.ServerPort != "" {
+		instance.ServerPort = fileConfig.ServerPort
+	}
+	if fileConfig.DBHost != "" {
+		instance.DBHost = fileConfig.DBHost
+	}
+	if fileConfig.DBPort != "" {
+		instance.DBPort = fileConfig.DBPort
+	}
+	if fileConfig.LogLevel != "" {
+		instance.LogLevel = fileConfig.LogLevel
+	}
 }
