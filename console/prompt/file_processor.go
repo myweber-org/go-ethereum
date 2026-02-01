dfa91760c74d96f0ea1326bf2d6887a332230263
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-01 23:52:59 +0000

    Add file processing utility with concurrency support

---
+++ console/prompt/file_processor.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type FileProcessor struct {
+	mu       sync.Mutex
+	results  map[string]int
+	wg       sync.WaitGroup
+}
+
+func NewFileProcessor() *FileProcessor {
+	return &FileProcessor{
+		results: make(map[string]int),
+	}
+}
+
+func (fp *FileProcessor) ProcessFile(path string) error {
+	fp.wg.Add(1)
+	go func() {
+		defer fp.wg.Done()
+		
+		file, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("Error opening file %s: %v\n", path, err)
+			return
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		lineCount := 0
+		for scanner.Scan() {
+			lineCount++
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error scanning file %s: %v\n", path, err)
+			return
+		}
+
+		fp.mu.Lock()
+		fp.results[path] = lineCount
+		fp.mu.Unlock()
+	}()
+
+	return nil
+}
+
+func (fp *FileProcessor) Wait() {
+	fp.wg.Wait()
+}
+
+func (fp *FileProcessor) GetResults() map[string]int {
+	return fp.results
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_processor <directory>")
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+	processor := NewFileProcessor()
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".txt" {
+			processor.ProcessFile(path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	processor.Wait()
+
+	results := processor.GetResults()
+	fmt.Println("File processing results:")
+	for file, lines := range results {
+		fmt.Printf("%s: %d lines\n", file, lines)
+	}
+}
