15c3d6433adf78c3cff1d05517014a4d56253652
heyQuim <hey.quim@hotmail.com>
2026-02-01 23:55:43 +0000

    Add user activity logging middleware for tracking API usage

--- accounts/keystore/testdata/v1/user_activity_logger.go
+++ accounts/keystore/testdata/v1/user_activity_logger.go
@@ -16,13 +16,32 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	al.handler.ServeHTTP(w, r)
+	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
 	duration := time.Since(start)
-
-	log.Printf("Activity: %s %s from %s took %v",
+	
+	log.Printf(
+		"%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		recorder.statusCode,
 		duration,
+		r.RemoteAddr,
 	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
