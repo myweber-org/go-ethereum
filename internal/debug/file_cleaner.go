38ad489f829c5714d7c7f23f650a7dc85e9d2bf9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-02 18:49:30 +0000

    Add utility to remove temporary files older than 7 days

--- internal/debug/file_cleaner.go
+++ internal/debug/file_cleaner.go
@@ -1,36 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"time"
+    "os"
+    "path/filepath"
+    "time"
 )
 
 func main() {
-	tempDir := os.TempDir()
-	cutoff := time.Now().AddDate(0, 0, -7)
-	var removedCount int
+    dir := "/tmp"
+    cutoff := time.Now().AddDate(0, 0, -7)
 
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-		if info.IsDir() {
-			return nil
-		}
-		if info.ModTime().Before(cutoff) {
-			if err := os.Remove(path); err == nil {
-				removedCount++
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		fmt.Printf("Error walking directory: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Cleaned %d temporary files older than 7 days\n", removedCount)
+    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
 }
