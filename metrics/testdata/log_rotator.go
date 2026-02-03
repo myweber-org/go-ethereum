667e3a281f4742357f4113c830e82dc325a8e9c4
heyQuim <hey.quim@hotmail.com>
2026-02-03 23:23:09 +0000

    Implement a basic log rotation mechanism with size-based file rotation

--- metrics/testdata/log_rotator.go
+++ metrics/testdata/log_rotator.go
@@ -295,4 +295,121 @@ func main() {
 	}
 	
 	fmt.Println("Log rotation test completed")
+}package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
+)
+
+type LogRotator struct {
+    mu          sync.Mutex
+    filePath    string
+    maxSize     int64
+    currentSize int64
+    file        *os.File
+}
+
+func NewLogRotator(filePath string, maxSizeMB int) (*LogRotator, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return nil, err
+    }
+
+    return &LogRotator{
+        filePath:    filePath,
+        maxSize:     maxSize,
+        currentSize: info.Size(),
+        file:        file,
+    }, nil
+}
+
+func (lr *LogRotator) Write(p []byte) (int, error) {
+    lr.mu.Lock()
+    defer lr.mu.Unlock()
+
+    if lr.currentSize+int64(len(p)) > lr.maxSize {
+        if err := lr.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := lr.file.Write(p)
+    if err == nil {
+        lr.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (lr *LogRotator) rotate() error {
+    if lr.file != nil {
+        lr.file.Close()
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    backupPath := fmt.Sprintf("%s.%s", lr.filePath, timestamp)
+    if err := os.Rename(lr.filePath, backupPath); err != nil {
+        return err
+    }
+
+    file, err := os.OpenFile(lr.filePath, os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    lr.file = file
+    lr.currentSize = 0
+    return nil
+}
+
+func (lr *LogRotator) Close() error {
+    lr.mu.Lock()
+    defer lr.mu.Unlock()
+    if lr.file != nil {
+        return lr.file.Close()
+    }
+    return nil
+}
+
+func (lr *LogRotator) CleanupOldLogs(maxBackups int) error {
+    lr.mu.Lock()
+    defer lr.mu.Unlock()
+
+    dir := filepath.Dir(lr.filePath)
+    base := filepath.Base(lr.filePath)
+
+    entries, err := os.ReadDir(dir)
+    if err != nil {
+        return err
+    }
+
+    var backups []string
+    for _, entry := range entries {
+        if !entry.IsDir() && len(entry.Name()) > len(base) && entry.Name()[:len(base)] == base {
+            backups = append(backups, entry.Name())
+        }
+    }
+
+    if len(backups) <= maxBackups {
+        return nil
+    }
+
+    for i := 0; i < len(backups)-maxBackups; i++ {
+        path := filepath.Join(dir, backups[i])
+        if err := os.Remove(path); err != nil {
+            return err
+        }
+    }
+    return nil
 }
