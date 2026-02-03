5f74c371a877b47b4bbfa8d110a9e1b676d4fe67
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-03 03:03:01 +0000

    Add user activity logging middleware for tracking API usage

--- eth/tracers/js/internal/user_activity_logger.go
+++ eth/tracers/js/internal/user_activity_logger.go
@@ -16,11 +16,9 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
 	al.handler.ServeHTTP(w, r)
-	
 	duration := time.Since(start)
-	
+
 	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
