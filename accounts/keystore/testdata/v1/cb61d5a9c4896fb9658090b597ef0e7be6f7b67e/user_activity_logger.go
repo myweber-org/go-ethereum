38355d406a70737b12070df0d18e9b460adf3794
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-04 16:23:19 +0000

    Add user activity logging middleware for API endpoints

--- accounts/keystore/testdata/v1/cb61d5a9c4896fb9658090b597ef0e7be6f7b67e/user_activity_logger.go
+++ accounts/keystore/testdata/v1/cb61d5a9c4896fb9658090b597ef0e7be6f7b67e/user_activity_logger.go
@@ -31,4 +31,58 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		userAgent,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		clientIP := r.RemoteAddr
+		userAgent := r.UserAgent()
+		method := r.Method
+		path := r.URL.Path
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(start)
+		status := recorder.statusCode
+
+		al.Logger.Printf(
+			"%s - %s %s - %d - %s - %v",
+			clientIP,
+			method,
+			path,
+			status,
+			userAgent,
+			duration,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
