75724b06a7b308ca0528f144f5ae33f91cf49824
heyQuim <hey.quim@hotmail.com>
2026-02-05 01:49:21 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- tests/fuzzers/bls12381/testdata/data_cleaner.go
+++ tests/fuzzers/bls12381/testdata/data_cleaner.go
@@ -1,24 +1,61 @@
 
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
 
-func removeDuplicates(input []int) []int {
-	seen := make(map[int]bool)
-	result := []int{}
+func removeDuplicates(inputFile, outputFile string) error {
+	in, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
 
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
+	reader := csv.NewReader(in)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		if len(record) == 0 {
+			continue
+		}
+		key := record[0]
+		if !seen[key] {
+			seen[key] = true
+			uniqueRecords = append(uniqueRecords, record)
 		}
 	}
-	return result
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	writer := csv.NewWriter(out)
+	return writer.WriteAll(uniqueRecords)
 }
 
 func main() {
-	slice := []int{5, 2, 8, 2, 5, 1, 8, 9}
-	uniqueSlice := removeDuplicates(slice)
-	fmt.Println("Original:", slice)
-	fmt.Println("Unique:", uniqueSlice)
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	err := removeDuplicates(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Duplicate removal completed successfully")
 }
