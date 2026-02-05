68c2dbd187407311b0387365e3c457299e8dc6eb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-05 23:33:54 +0000

    Add user activity logging middleware with rate limiting

--- cmd/devp2p/internal/v5test/user_activity_logger.go
+++ cmd/devp2p/internal/v5test/user_activity_logger.go
@@ -29,4 +29,42 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func WithActivityLogging(next http.Handler) http.Handler {
 	return NewActivityLogger(next)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type ActivityLogger struct {
+	limiter *rate.Limiter
+}
+
+func NewActivityLogger(rps int) *ActivityLogger {
+	return &ActivityLogger{
+		limiter: rate.NewLimiter(rate.Limit(rps), rps),
+	}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !al.limiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+
+		log.Printf("Activity: %s %s from %s took %v",
+			r.Method,
+			r.URL.Path,
+			r.RemoteAddr,
+			duration,
+		)
+	})
 }
