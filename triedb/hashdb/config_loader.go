d11ea32fa9c0640db1ccb574d927442e1c61b92d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-06 16:42:52 +0000

    Add configuration loader with YAML parsing support

---
+++ triedb/hashdb/config_loader.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    Logging struct {
+        Level  string `yaml:"level"`
+        Output string `yaml:"output"`
+    } `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+    if configPath == "" {
+        return nil, fmt.Errorf("config path cannot be empty")
+    }
+
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open config file: %w", err)
+    }
+    defer file.Close()
+
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML config: %w", err)
+    }
+
+    if config.Server.Host == "" {
+        config.Server.Host = "localhost"
+    }
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+    if config.Logging.Level == "" {
+        config.Logging.Level = "info"
+    }
+
+    return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) error {
+    if config.Server.Port < 1 || config.Server.Port > 65535 {
+        return fmt.Errorf("invalid server port: %d", config.Server.Port)
+    }
+    if config.Database.Host == "" {
+        return fmt.Errorf("database host is required")
+    }
+    if config.Database.Name == "" {
+        return fmt.Errorf("database name is required")
+    }
+    return nil
+}
