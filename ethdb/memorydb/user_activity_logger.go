f2aef9298aed367db50e6b288cf1f4d0822c9e9b
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-07 02:27:06 +0000

    Add user activity logging middleware for HTTP requests

--- ethdb/memorydb/user_activity_logger.go
+++ ethdb/memorydb/user_activity_logger.go
@@ -115,4 +115,44 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(statusCode int) {
 	rr.statusCode = statusCode
 	rr.ResponseWriter.WriteHeader(statusCode)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
+	duration := time.Since(start)
+	log.Printf("%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		r.RemoteAddr,
+		duration,
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
