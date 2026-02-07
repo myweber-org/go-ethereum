f3ea0fc05b9757cccd69dc06d19ea43d488abf7f
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 00:03:31 +0000

    Add user activity logging middleware for API endpoints

--- triedb/database/user_activity_logger.go
+++ triedb/database/user_activity_logger.go
@@ -16,30 +16,13 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-
-	al.handler.ServeHTTP(recorder, r)
-
+	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
+
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
-		recorder.statusCode,
+		r.RemoteAddr,
 		duration,
-		r.UserAgent(),
 	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
 }
