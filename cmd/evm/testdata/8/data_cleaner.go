7d60cc4e7c88037c5d17e00a471589441c460c3c
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-07 01:45:16 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

---
+++ cmd/evm/testdata/8/data_cleaner.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func removeDuplicates(inputFile, outputFile string) error {
+	in, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	reader := csv.NewReader(in)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		key := strings.Join(record, "|")
+		if !seen[key] {
+			seen[key] = true
+			uniqueRecords = append(uniqueRecords, record)
+		}
+	}
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	writer := csv.NewWriter(out)
+	return writer.WriteAll(uniqueRecords)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	err := removeDuplicates(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Duplicate removal completed successfully")
+}
