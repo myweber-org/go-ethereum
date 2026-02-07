7b3e3a591f68ee7c1b81d08a08fc9deba4fd8303
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-07 01:40:57 +0000

    Add utility function to calculate moving average of a slice

--- crypto/secp256k1/libsecp256k1/src/java/data_processor.go
+++ crypto/secp256k1/libsecp256k1/src/java/data_processor.go
@@ -1,12 +1,27 @@
 
-package data
+package main
 
-func FilterAndTransform(numbers []int, predicate func(int) bool, transform func(int) int) []int {
-    var result []int
-    for _, num := range numbers {
-        if predicate(num) {
-            result = append(result, transform(num))
-        }
+import "fmt"
+
+func MovingAverage(data []float64, window int) []float64 {
+    if window <= 0 || window > len(data) {
+        return nil
+    }
+    result := make([]float64, len(data)-window+1)
+    var sum float64
+    for i := 0; i < window; i++ {
+        sum += data[i]
+    }
+    result[0] = sum / float64(window)
+    for i := window; i < len(data); i++ {
+        sum = sum - data[i-window] + data[i]
+        result[i-window+1] = sum / float64(window)
     }
     return result
+}
+
+func main() {
+    sample := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0}
+    avg := MovingAverage(sample, 3)
+    fmt.Println("Moving average with window 3:", avg)
 }
