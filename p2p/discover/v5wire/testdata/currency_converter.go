d43a465e098b22ed86be42235b011f78359ff1f2
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-07 02:48:35 +0000

    Add currency conversion utility with USD to EUR rate

---
+++ p2p/discover/v5wire/testdata/currency_converter.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+const usdToEurRate = 0.85
+
+func ConvertUSDToEUR(amount float64) float64 {
+	return amount * usdToEurRate
+}
+
+func main() {
+	usdAmount := 100.0
+	eurAmount := ConvertUSDToEUR(usdAmount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
