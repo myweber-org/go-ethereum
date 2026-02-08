7983bad074b5cb9e8b50817ea758c1706b33bc15
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-08 20:12:37 +0000

    Add user activity logging middleware with request details and timestamp

--- metrics/influxdb/testdata/user_activity_logger.go
+++ metrics/influxdb/testdata/user_activity_logger.go
@@ -49,4 +49,55 @@ func main() {
     }
 
     log.Println("Activity logging completed")
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp  time.Time
+	Method     string
+	Path       string
+	RemoteAddr string
+	UserAgent  string
+	StatusCode int
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		lw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		
+		next.ServeHTTP(lw, r)
+		
+		activity := ActivityLog{
+			Timestamp:  start,
+			Method:     r.Method,
+			Path:       r.URL.Path,
+			RemoteAddr: r.RemoteAddr,
+			UserAgent:  r.UserAgent(),
+			StatusCode: lw.statusCode,
+		}
+		
+		log.Printf("ACTIVITY: %s %s %d %s %s %v",
+			activity.Method,
+			activity.Path,
+			activity.StatusCode,
+			activity.RemoteAddr,
+			activity.UserAgent,
+			activity.Timestamp.Format(time.RFC3339))
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
