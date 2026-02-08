77e208c4a19639207413180a235e6a88168f854d
ElaraWrites <elara.writes@outlook.com>
2026-02-08 16:51:04 +0000

    Add user activity logging middleware for tracking API usage

--- crypto/kzg4844/user_activity_logger.go
+++ crypto/kzg4844/user_activity_logger.go
@@ -32,4 +32,50 @@ func extractUserID(r *http.Request) string {
 		return "authenticated_user"
 	}
 	return "anonymous_user"
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf(
+		"Method: %s | Path: %s | Status: %d | Duration: %v | UserAgent: %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.UserAgent(),
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
