2a8461400fe6ec3959aa9bbf0db5e51a7822aa89
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-09 21:01:21 +0000

    Add file encryption utility with AES-256-CBC support

--- crypto/ecies/file_encryption.go
+++ crypto/ecies/file_encryption.go
@@ -93,4 +93,73 @@ func main() {
 	}
 
 	fmt.Println("File encryption/decryption completed successfully")
+}package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "fmt"
+    "io"
+)
+
+func encrypt(plaintext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return nil, err
+    }
+
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+    return ciphertext, nil
+}
+
+func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    stream := cipher.NewCFBDecrypter(block, iv)
+    stream.XORKeyStream(ciphertext, ciphertext)
+
+    return ciphertext, nil
+}
+
+func main() {
+    key := make([]byte, 32)
+    if _, err := io.ReadFull(rand.Reader, key); err != nil {
+        panic(err)
+    }
+
+    secretMessage := "This is a confidential message"
+    fmt.Printf("Original: %s\n", secretMessage)
+
+    encrypted, err := encrypt([]byte(secretMessage), key)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Encrypted (base64): %s\n", base64.StdEncoding.EncodeToString(encrypted))
+
+    decrypted, err := decrypt(encrypted, key)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Decrypted: %s\n", decrypted)
 }
