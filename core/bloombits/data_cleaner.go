b9f91803f5483927e144f66bf9fa268945724719
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-09 22:13:07 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- core/bloombits/data_cleaner.go
+++ core/bloombits/data_cleaner.go
@@ -56,4 +56,26 @@ func main() {
 	cleaner.Reset()
 	testValue := "  TEST  "
 	fmt.Printf("Normalized '%s': '%s'\n", testValue, cleaner.Normalize(testValue))
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
 }
