b2dd3f4dcd6f31d9af9972f065f5c548dc2d0c7a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-10 19:58:17 +0000

    Add configuration loader with YAML parsing support

---
+++ rlp/config_loader.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+	Server struct {
+		Port    int    `yaml:"port"`
+		Host    string `yaml:"host"`
+		Timeout int    `yaml:"timeout"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		log.Printf("Invalid server port: %d", config.Server.Port)
+		return false
+	}
+
+	if config.Database.Host == "" {
+		log.Print("Database host cannot be empty")
+		return false
+	}
+
+	if config.Logging.Level != "debug" && config.Logging.Level != "info" && config.Logging.Level != "error" {
+		log.Printf("Invalid logging level: %s", config.Logging.Level)
+		return false
+	}
+
+	return true
+}
