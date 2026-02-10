67e8dce55764d46be12068dc606c7c2c5bf51373
InkspillVerse <inkspill.verse@outlook.com>
2026-02-10 16:28:05 +0000

    Add utility to remove temporary files older than 7 days

--- console/testdata/file_cleaner.go
+++ console/testdata/file_cleaner.go
@@ -39,4 +39,48 @@ func cleanOldFiles(dirPath string, days int) error {
 		}
 	}
 	return nil
+}package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_temp"
+	retentionDays = 7
+)
+
+func main() {
+	if err := cleanOldFiles(); err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles() error {
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
+	return filepath.Walk(tempDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			if err := os.Remove(path); err != nil {
+				fmt.Printf("Failed to remove %s: %v\n", path, err)
+			} else {
+				fmt.Printf("Removed old file: %s\n", path)
+			}
+		}
+		return nil
+	})
 }
