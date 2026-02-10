99f94b0f89db0b1041a73039981100698aae72d7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-10 15:09:01 +0000

    Add file encryption utility with AES-GCM and key derivation

---
+++ cmd/evm/testdata/5/file_encryptor.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptData(plaintext []byte, passphrase string) ([]byte, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return nil, err
+	}
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return append(salt, ciphertext...), nil
+}
+
+func decryptData(ciphertext []byte, passphrase string) ([]byte, error) {
+	if len(ciphertext) < 16 {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	salt := ciphertext[:16]
+	ciphertext = ciphertext[16:]
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryptor.go <encrypt|decrypt> <input_file> <output_file>")
+		fmt.Println("Passphrase will be read from environment variable ENCRYPTION_KEY")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	passphrase := os.Getenv("ENCRYPTION_KEY")
+	if passphrase == "" {
+		fmt.Println("Error: ENCRYPTION_KEY environment variable not set")
+		os.Exit(1)
+	}
+
+	inputData, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading input file: %v\n", err)
+		os.Exit(1)
+	}
+
+	var outputData []byte
+	switch operation {
+	case "encrypt":
+		outputData, err = encryptData(inputData, passphrase)
+	case "decrypt":
+		outputData, err = decryptData(inputData, passphrase)
+	default:
+		fmt.Println("Error: operation must be 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("Error during %s: %v\n", operation, err)
+		os.Exit(1)
+	}
+
+	if err := os.WriteFile(outputFile, outputData, 0644); err != nil {
+		fmt.Printf("Error writing output file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Operation %s completed successfully\n", operation)
+}
