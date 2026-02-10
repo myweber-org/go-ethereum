f0cdf0d5ba5906e5834f4f67b2504e4db1c55fda
FelixMakes <felix.makes@hotmail.com>
2026-02-10 22:51:39 +0000

    Add user activity logging with timestamp and action type

---
+++ docs/user_activity_logger.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type Activity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, action, details string) error {
+    activity := Activity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(activity)
+    if err != nil {
+        return err
+    }
+
+    _, err = al.logFile.Write(append(data, '\n'))
+    return err
+}
+
+func (al *ActivityLogger) Close() error {
+    return al.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    err = logger.LogActivity("user456", "file_upload", "uploaded profile.jpg")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    fmt.Println("Activity logging completed")
+}
