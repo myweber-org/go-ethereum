2014c518bd2c6c1e4289e25051e51cf9c84671f2
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 23:24:00 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ cmd/geth/testdata/data_cleaner.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
