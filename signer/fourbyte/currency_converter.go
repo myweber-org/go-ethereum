189205e12b8cfbb587613a85d738e208e55e1853
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-11 19:06:47 +0000

    Add currency conversion utility with USD to EUR rate

--- signer/fourbyte/currency_converter.go
+++ signer/fourbyte/currency_converter.go
@@ -76,3 +76,19 @@ func main() {
 
 	fmt.Printf("%.2f %s = %.2f %s\n", amount, fromCurrency, result, toCurrency)
 }
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const usdToEurRate = 0.85
+	var usdAmount float64
+
+	fmt.Print("Enter amount in USD: ")
+	fmt.Scan(&usdAmount)
+
+	eurAmount := usdAmount * usdToEurRate
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
