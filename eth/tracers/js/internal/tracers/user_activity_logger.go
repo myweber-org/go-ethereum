0a3783d3221648b55448bc8e804e92c50a2e7594
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-12 20:18:56 +0000

    Add user activity logging with timestamp and action type

--- eth/tracers/js/internal/tracers/user_activity_logger.go
+++ eth/tracers/js/internal/tracers/user_activity_logger.go
@@ -24,4 +24,81 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(start)
 	log.Printf("User activity: %s %s | IP: %s | Agent: %s | Duration: %v",
 		r.Method, r.URL.Path, ipAddress, userAgent, duration)
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Resource  string    `json:"resource"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, action, resource string) error {
+	activity := ActivityLog{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Resource:  resource,
+	}
+
+	data, err := json.Marshal(activity)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = al.logFile.Write(data)
+	return err
+}
+
+func (al *ActivityLogger) Close() error {
+	return al.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID   string
+		action   string
+		resource string
+	}{
+		{"user_001", "LOGIN", "auth_system"},
+		{"user_002", "CREATE", "document_123"},
+		{"user_001", "UPDATE", "profile_settings"},
+		{"user_003", "DELETE", "comment_456"},
+	}
+
+	for _, act := range activities {
+		err := logger.LogActivity(act.userID, act.action, act.resource)
+		if err != nil {
+			log.Printf("Failed to log activity: %v", err)
+		} else {
+			fmt.Printf("Logged: %s performed %s on %s\n", act.userID, act.action, act.resource)
+		}
+	}
 }
