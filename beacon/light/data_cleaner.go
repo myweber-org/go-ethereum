343433452846bbb9524ad2f59201743945f2d3e9
heyQuim <hey.quim@hotmail.com>
2026-02-12 16:11:07 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- beacon/light/data_cleaner.go
+++ beacon/light/data_cleaner.go
@@ -240,4 +240,26 @@ func main() {
 	for _, v := range valid {
 		fmt.Printf("  - %s\n", v)
 	}
+}package main
+
+import "fmt"
+
+func removeDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 6, 6}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
 }
