d11452aa49e2ba8883a20565c520bbba034faa19
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-12 23:44:48 +0000

    Add user activity logging middleware for API endpoints

--- ethdb/leveldb/user_activity_logger.go
+++ ethdb/leveldb/user_activity_logger.go
@@ -16,66 +16,15 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+	
 	al.handler.ServeHTTP(w, r)
+	
 	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | RemoteAddr: %s",
+	
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
-		duration,
 		r.RemoteAddr,
-	)
-}
-package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	UserID    string
-	Endpoint  string
-	Method    string
-	Timestamp time.Time
-	IPAddress string
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserID(authHeader)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			Endpoint:  r.URL.Path,
-			Method:    r.Method,
-			Timestamp: start,
-			IPAddress: r.RemoteAddr,
-		}
-
-		logActivity(activity)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extractUserID(token string) string {
-	return "user123"
-}
-
-func logActivity(activity ActivityLog) {
-	log.Printf("Activity: %s %s by %s from %s at %v",
-		activity.Method,
-		activity.Endpoint,
-		activity.UserID,
-		activity.IPAddress,
-		activity.Timestamp.Format(time.RFC3339),
+		duration,
 	)
 }
