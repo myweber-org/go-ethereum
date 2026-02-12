bc5286925202956b10ffc2ffba83aa58fdb98110
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-12 23:10:22 +0000

    Add data processing function to calculate average from slice

--- eth/ethconfig/data_processor.go
+++ eth/ethconfig/data_processor.go
@@ -46,4 +46,25 @@ func ProcessUserInput(email, username string, age int) (UserData, error) {
 		return UserData{}, err
 	}
 	return userData, nil
+}package main
+
+import "fmt"
+
+func calculateAverage(numbers []float64) float64 {
+    if len(numbers) == 0 {
+        return 0
+    }
+    
+    var sum float64
+    for _, num := range numbers {
+        sum += num
+    }
+    
+    return sum / float64(len(numbers))
+}
+
+func main() {
+    data := []float64{10.5, 20.3, 15.7, 8.9, 12.1}
+    avg := calculateAverage(data)
+    fmt.Printf("Average: %.2f\n", avg)
 }
