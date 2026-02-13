d764101ea049ef885befbd0c3489f1c728b4eba4
FelixMakes <felix.makes@hotmail.com>
2026-02-13 16:54:29 +0000

    Add user activity logging middleware for API endpoints

--- cmd/evm/testdata/19/user_activity_logger.go
+++ cmd/evm/testdata/19/user_activity_logger.go
@@ -66,4 +66,66 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	UserID    string
+	Endpoint  string
+	Method    string
+	Timestamp time.Time
+	IPAddress string
+}
+
+var activityChannel = make(chan ActivityLog, 100)
+
+func init() {
+	go processActivityLogs()
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		userID := "anonymous"
+		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
+			userID = extractUserID(authHeader)
+		}
+
+		activity := ActivityLog{
+			UserID:    userID,
+			Endpoint:  r.URL.Path,
+			Method:    r.Method,
+			Timestamp: start,
+			IPAddress: r.RemoteAddr,
+		}
+
+		select {
+		case activityChannel <- activity:
+		default:
+			log.Println("Activity log channel full, dropping log entry")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func extractUserID(token string) string {
+	return "user_" + token[:8]
+}
+
+func processActivityLogs() {
+	for activity := range activityChannel {
+		log.Printf("ACTIVITY: %s %s %s %s %v\n",
+			activity.UserID,
+			activity.IPAddress,
+			activity.Method,
+			activity.Endpoint,
+			activity.Timestamp.Format(time.RFC3339))
+	}
 }
