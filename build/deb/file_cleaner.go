98896f554fe7edcedece14bbac2a1efd86854634
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-13 16:24:50 +0000

    Add utility to remove temporary files older than 7 days

--- build/deb/file_cleaner.go
+++ build/deb/file_cleaner.go
@@ -61,4 +61,28 @@ func main() {
 	}
 
 	fmt.Printf("Successfully removed duplicates. Output written to %s\n", outputFile)
+}package main
+
+import (
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    tempDir := os.TempDir()
+    cutoff := time.Now().AddDate(0, 0, -7)
+
+    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
 }
