6e20ca789f6d308bdcec263abdd3533da0e5f963
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-13 20:03:32 +0000

    Add data cleaning utility to remove duplicate entries from a slice

---
+++ triedb/pathdb/data_cleaner.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
