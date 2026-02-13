7e2cd5901e3129951d66ac401f031353fa8e76e7
InkspillVerse <inkspill.verse@outlook.com>
2026-02-13 14:24:57 +0000

    Add AES encryption utility for secure data handling

--- ethclient/gethclient/file_encryptor.go
+++ ethclient/gethclient/file_encryptor.go
@@ -146,4 +146,55 @@ func main() {
 		fmt.Printf("Unknown command: %s\n", command)
 		os.Exit(1)
 	}
+}package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "io"
+)
+
+func encryptData(plainText string, key []byte) (string, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plainText))
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return "", err
+    }
+
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plainText))
+
+    return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decryptData(encryptedText string, key []byte) (string, error) {
+    ciphertext, err := base64.StdEncoding.DecodeString(encryptedText)
+    if err != nil {
+        return "", err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        return "", errors.New("ciphertext too short")
+    }
+
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    stream := cipher.NewCFBDecrypter(block, iv)
+    stream.XORKeyStream(ciphertext, ciphertext)
+
+    return string(ciphertext), nil
 }
