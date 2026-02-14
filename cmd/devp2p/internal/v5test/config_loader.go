78b62801f6d47f9c631c0419bb74ab97119768ce
InkspillVerse <inkspill.verse@outlook.com>
2026-02-14 00:28:28 +0000

    Add configuration loader with YAML parsing and environment variable support

--- cmd/devp2p/internal/v5test/config_loader.go
+++ cmd/devp2p/internal/v5test/config_loader.go
@@ -51,4 +51,96 @@ func validateConfig(c *Config) error {
         return fmt.Errorf("invalid log level: %s", c.Logging.Level)
     }
     return nil
+}package config
+
+import (
+    "fmt"
+    "os"
+    "strings"
+
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host" env:"SERVER_HOST"`
+        Port int    `yaml:"port" env:"SERVER_PORT"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host" env:"DB_HOST"`
+        Port     int    `yaml:"port" env:"DB_PORT"`
+        Name     string `yaml:"name" env:"DB_NAME"`
+        User     string `yaml:"user" env:"DB_USER"`
+        Password string `yaml:"password" env:"DB_PASSWORD"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+    config := &Config{}
+    
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open config file: %w", err)
+    }
+    defer file.Close()
+
+    decoder := yaml.NewDecoder(file)
+    if err := decoder.Decode(config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML config: %w", err)
+    }
+
+    overrideFromEnv(config)
+    
+    return config, nil
+}
+
+func overrideFromEnv(config *Config) {
+    overrideStruct(config, "")
+}
+
+func overrideStruct(s interface{}, prefix string) {
+    val := reflect.ValueOf(s).Elem()
+    typ := val.Type()
+
+    for i := 0; i < val.NumField(); i++ {
+        field := val.Field(i)
+        fieldType := typ.Field(i)
+
+        envTag := fieldType.Tag.Get("env")
+        yamlTag := fieldType.Tag.Get("yaml")
+
+        if field.Kind() == reflect.Struct {
+            nestedPrefix := prefix
+            if yamlTag != "" {
+                nestedPrefix = strings.TrimSuffix(prefix+"_"+strings.ToUpper(yamlTag), "_")
+            }
+            overrideStruct(field.Addr().Interface(), nestedPrefix)
+            continue
+        }
+
+        if envTag == "" {
+            continue
+        }
+
+        envKey := envTag
+        if prefix != "" {
+            envKey = prefix + "_" + envKey
+        }
+
+        if envValue, exists := os.LookupEnv(envKey); exists {
+            switch field.Kind() {
+            case reflect.String:
+                field.SetString(envValue)
+            case reflect.Int:
+                if intValue, err := strconv.Atoi(envValue); err == nil {
+                    field.SetInt(int64(intValue))
+                }
+            case reflect.Bool:
+                if boolValue, err := strconv.ParseBool(envValue); err == nil {
+                    field.SetBool(boolValue)
+                }
+            }
+        }
+    }
 }
