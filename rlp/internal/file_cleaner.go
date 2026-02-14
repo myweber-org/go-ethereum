03b68bcb69671e09c0d26e7744ea93168c9283d8
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-14 15:22:28 +0000

    Add utility to remove temporary files older than 7 days

---
+++ rlp/internal/file_cleaner.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	tempDir := os.TempDir()
+	cutoff := time.Now().AddDate(0, 0, -7)
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.ModTime().Before(cutoff) && !info.IsDir() {
+			fmt.Printf("Removing old file: %s\n", path)
+			os.Remove(path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Error cleaning temp files: %v\n", err)
+	}
+}
