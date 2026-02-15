7c8fc56105985da5453ba45c1088ecf7d62807c5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-15 21:45:05 +0000

    Add currency conversion utility with USD to EUR rate

--- rlp/rlpgen/testdata/currency_converter.go
+++ rlp/rlpgen/testdata/currency_converter.go
@@ -50,3 +50,19 @@ func main() {
 	eurAmount := ConvertUSDToEUR(usdAmount)
 	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
 }
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const usdToEurRate = 0.85
+	var usdAmount float64
+
+	fmt.Print("Enter amount in USD: ")
+	fmt.Scan(&usdAmount)
+
+	eurAmount := usdAmount * usdToEurRate
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
