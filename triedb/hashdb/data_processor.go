2f568dc5692139d340ea8b7d4a7841c95d0aabaa
FelixMakes <felix.makes@hotmail.com>
2026-02-17 00:39:48 +0000

    Add utility function to filter and transform numeric slices

--- triedb/hashdb/data_processor.go
+++ triedb/hashdb/data_processor.go
@@ -157,3 +157,26 @@ func ProcessUserInput(rawName string, rawEmail string, rawAge int) (UserData, er
 
 	return user, nil
 }
+package main
+
+import (
+	"fmt"
+)
+
+// FilterAndDouble filters even numbers from a slice and doubles their values.
+func FilterAndDouble(numbers []int) []int {
+	var result []int
+	for _, num := range numbers {
+		if num%2 == 0 {
+			result = append(result, num*2)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	output := FilterAndDouble(input)
+	fmt.Println("Original:", input)
+	fmt.Println("Filtered and Doubled:", output)
+}
