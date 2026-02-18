26dd8d2d147ed96a7b08b98999823c16f8a57181
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-18 19:27:44 +0000

    Add user activity logging middleware for API endpoints

---
+++ build/tools/user_activity_logger.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf("Activity: %s %s from %s took %v", r.Method, r.URL.Path, r.RemoteAddr, duration)
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("API response"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", apiHandler)
+
+	wrappedMux := NewActivityLogger(mux)
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", wrappedMux))
+}
