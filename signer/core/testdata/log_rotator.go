dd8c38d771a8f8fb2c64fd6b259b01ec2427b182
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-18 19:14:08 +0000

    Implement a basic log rotation mechanism that archives old logs and starts a new log file

--- signer/core/testdata/log_rotator.go
+++ signer/core/testdata/log_rotator.go
@@ -120,4 +120,65 @@ func main() {
         }
         time.Sleep(10 * time.Millisecond)
     }
+}package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type LogRotator struct {
+	CurrentLogPath string
+	MaxSize        int64
+	ArchiveDir     string
+}
+
+func NewLogRotator(logPath string, maxSize int64, archiveDir string) *LogRotator {
+	return &LogRotator{
+		CurrentLogPath: logPath,
+		MaxSize:        maxSize,
+		ArchiveDir:     archiveDir,
+	}
+}
+
+func (lr *LogRotator) CheckAndRotate() error {
+	fileInfo, err := os.Stat(lr.CurrentLogPath)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed to stat log file: %w", err)
+	}
+
+	if fileInfo.Size() < lr.MaxSize {
+		return nil
+	}
+
+	timestamp := time.Now().Format("20060102_150405")
+	archiveName := fmt.Sprintf("log_%s.log", timestamp)
+	archivePath := filepath.Join(lr.ArchiveDir, archiveName)
+
+	err = os.Rename(lr.CurrentLogPath, archivePath)
+	if err != nil {
+		return fmt.Errorf("failed to rotate log: %w", err)
+	}
+
+	newFile, err := os.Create(lr.CurrentLogPath)
+	if err != nil {
+		return fmt.Errorf("failed to create new log file: %w", err)
+	}
+	newFile.Close()
+
+	fmt.Printf("Log rotated: %s -> %s\n", lr.CurrentLogPath, archivePath)
+	return nil
+}
+
+func main() {
+	rotator := NewLogRotator("app.log", 1024*1024, "./archive")
+	err := rotator.CheckAndRotate()
+	if err != nil {
+		fmt.Printf("Error during log rotation: %v\n", err)
+	}
 }
