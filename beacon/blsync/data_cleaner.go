d7589deaa7058e8fe9e91cda334f7eed33d97aef
FelixMakes <felix.makes@hotmail.com>
2026-02-19 16:34:15 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ beacon/blsync/data_cleaner.go
@@ -0,0 +1,15 @@
+package datautils
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+
+	return result
+}
