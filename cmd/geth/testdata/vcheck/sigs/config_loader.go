ecd19005a9e877fcea5533ea0c615ba85296ee31
FelixMakes <felix.makes@hotmail.com>
2026-02-21 22:19:38 +0000

    Add configuration loader with environment variable support and validation

--- cmd/geth/testdata/vcheck/sigs/config_loader.go
+++ cmd/geth/testdata/vcheck/sigs/config_loader.go
@@ -60,4 +60,54 @@ func DefaultConfigPath() string {
         return "./config.yaml"
     }
     return filepath.Join(homeDir, ".app", "config.yaml")
+}package config
+
+import (
+    "os"
+    "strconv"
+    "strings"
+)
+
+type Config struct {
+    ServerPort int
+    DBHost     string
+    DBPort     int
+    DebugMode  bool
+    AllowedIPs []string
+}
+
+func LoadConfig() (*Config, error) {
+    cfg := &Config{}
+
+    port, err := strconv.Atoi(getEnv("SERVER_PORT", "8080"))
+    if err != nil {
+        return nil, err
+    }
+    cfg.ServerPort = port
+
+    cfg.DBHost = getEnv("DB_HOST", "localhost")
+
+    dbPort, err := strconv.Atoi(getEnv("DB_PORT", "5432"))
+    if err != nil {
+        return nil, err
+    }
+    cfg.DBPort = dbPort
+
+    debug, err := strconv.ParseBool(getEnv("DEBUG_MODE", "false"))
+    if err != nil {
+        return nil, err
+    }
+    cfg.DebugMode = debug
+
+    ips := getEnv("ALLOWED_IPS", "127.0.0.1,::1")
+    cfg.AllowedIPs = strings.Split(ips, ",")
+
+    return cfg, nil
+}
+
+func getEnv(key, defaultValue string) string {
+    if value := os.Getenv(key); value != "" {
+        return value
+    }
+    return defaultValue
 }
