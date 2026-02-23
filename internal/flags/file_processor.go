0cf0db4ce06b3d78700aa9cd8971884d72ede20c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 23:31:39 +0000

    Add utility functions for reading and writing JSON files with validation

--- internal/flags/file_processor.go
+++ internal/flags/file_processor.go
@@ -56,3 +56,71 @@ func main() {
 
 	fmt.Printf("Loaded config: %+v\n", loadedConfig)
 }
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ReadJSONFile(filename string, v interface{}) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+
+	if err := json.Unmarshal(data, v); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	return nil
+}
+
+func WriteJSONFile(filename string, v interface{}, indent bool) error {
+	var data []byte
+	var err error
+
+	if indent {
+		data, err = json.MarshalIndent(v, "", "  ")
+	} else {
+		data, err = json.Marshal(v)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := file.Write(data); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
+
+func ValidateJSONStructure(data []byte, expectedFields []string) bool {
+	var m map[string]interface{}
+	if err := json.Unmarshal(data, &m); err != nil {
+		return false
+	}
+
+	for _, field := range expectedFields {
+		if _, exists := m[field]; !exists {
+			return false
+		}
+	}
+	return true
+}
