7c933676d895464f9f4a6f07496c7599b9ae86d0
InkspillVerse <inkspill.verse@outlook.com>
2026-02-23 21:57:55 +0000

    Add user activity logging middleware for tracking API usage

--- rlp/internal/rlpstruct/user_activity_logger.go
+++ rlp/internal/rlpstruct/user_activity_logger.go
@@ -26,4 +26,33 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 		time.Now().Format(time.RFC3339),
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(startTime)
+	
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
