fa5e3a49886ffb6d23084d58fa4c69b3e2e35df0
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-23 21:07:56 +0000

    Add user activity logging middleware with request details and timestamp

--- ethdb/pebble/user_activity_logger.go
+++ ethdb/pebble/user_activity_logger.go
@@ -87,4 +87,42 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IP        string
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		activity := ActivityLog{
+			Timestamp: start,
+			Method:    r.Method,
+			Path:      r.URL.Path,
+			UserAgent: r.UserAgent(),
+			IP:        r.RemoteAddr,
+		}
+
+		log.Printf("Activity: %s %s from %s (%s) at %s",
+			activity.Method,
+			activity.Path,
+			activity.IP,
+			activity.UserAgent,
+			activity.Timestamp.Format(time.RFC3339),
+		)
+
+		next.ServeHTTP(w, r)
+	})
 }
