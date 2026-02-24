086d27825d27cd0449bdaaa6d54bcdaf8f245ab4
heyQuim <hey.quim@hotmail.com>
2026-02-24 23:58:18 +0000

    Add utility to remove temporary files older than 7 days

---
+++ p2p/simulations/file_cleaner.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    tempDir := os.TempDir()
+    cutoff := time.Now().AddDate(0, 0, -7)
+
+    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
+}
