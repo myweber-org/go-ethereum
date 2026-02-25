b1d2948f51ec768c9be137773b771b36639f6c30
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-25 19:23:19 +0000

    Add data processing function to calculate average from slice of integers

--- common/prque/data_processor.go
+++ common/prque/data_processor.go
@@ -147,3 +147,25 @@ func CalculateStatistics(records []DataRecord) (float64, float64) {
     average := sum / float64(len(records))
     return average, max
 }
+package main
+
+import "fmt"
+
+func calculateAverage(numbers []int) float64 {
+    if len(numbers) == 0 {
+        return 0
+    }
+    
+    sum := 0
+    for _, num := range numbers {
+        sum += num
+    }
+    
+    return float64(sum) / float64(len(numbers))
+}
+
+func main() {
+    data := []int{10, 20, 30, 40, 50}
+    avg := calculateAverage(data)
+    fmt.Printf("Average: %.2f\n", avg)
+}
