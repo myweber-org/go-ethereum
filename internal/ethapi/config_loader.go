51e87919fab8e9bb142a8e7b8801a0225c01ebe6
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-25 20:37:21 +0000

    Add configuration loader with YAML support and environment variable overrides

--- internal/ethapi/config_loader.go
+++ internal/ethapi/config_loader.go
@@ -120,4 +120,58 @@ func (c *Config) String() string {
 	safeConfig.DBPassword = "***REDACTED***"
 	data, _ := json.MarshalIndent(safeConfig, "", "  ")
 	return string(data)
+}package config
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		URL      string `yaml:"url"`
+		PoolSize int    `yaml:"pool_size"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(config)
+	return config, nil
+}
+
+func overrideFromEnv(c *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		c.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		if port, err := strconv.Atoi(val); err == nil {
+			c.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DATABASE_URL"); val != "" {
+		c.Database.URL = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		c.LogLevel = strings.ToUpper(val)
+	}
 }
