9096e2be640aa36e5631534ba83c6e6a76a074a9
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-25 20:16:41 +0000

    Add user activity logging with timestamp and action type

--- crypto/secp256k1/libsecp256k1/user_activity_logger.go
+++ crypto/secp256k1/libsecp256k1/user_activity_logger.go
@@ -88,4 +88,53 @@ func ActivityLogger(next http.Handler) http.Handler {
 
 func extractUserID(token string) string {
 	return "user_" + token[:8]
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+func logActivity(userID, action, details string) {
+    activity := ActivityLog{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    logEntry, err := json.Marshal(activity)
+    if err != nil {
+        log.Printf("Failed to marshal activity: %v", err)
+        return
+    }
+
+    fmt.Println(string(logEntry))
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Printf("Failed to open log file: %v", err)
+        return
+    }
+    defer file.Close()
+
+    if _, err := file.Write(append(logEntry, '\n')); err != nil {
+        log.Printf("Failed to write to log file: %v", err)
+    }
+}
+
+func main() {
+    logActivity("user123", "login", "User logged in from web browser")
+    logActivity("user456", "purchase", "Purchased item ID: 789")
+    logActivity("user123", "logout", "Session terminated")
 }
