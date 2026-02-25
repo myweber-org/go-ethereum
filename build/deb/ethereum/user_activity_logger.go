fea330acabd9ac24eda928548bfb757028c5fbe1
InkspillVerse <inkspill.verse@outlook.com>
2026-02-25 15:28:21 +0000

    Add user activity logging middleware with request details and timestamp

--- build/deb/ethereum/user_activity_logger.go
+++ build/deb/ethereum/user_activity_logger.go
@@ -27,4 +27,34 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("[%s] %s %s - %s - Duration: %v",
+		time.Now().Format("2006-01-02 15:04:05"),
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
