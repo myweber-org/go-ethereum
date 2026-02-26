a84c704ab13a32b29143025ce05fedcc75871c66
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 22:09:08 +0000

    Add data validation and sanitization functions for user input

---
+++ crypto/signify/data_processor.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TrimAndLower(input string) string {
+	return strings.ToLower(strings.TrimSpace(input))
+}
