56784dfcd226d4b073810c418de97f843a55ee4b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-27 15:17:57 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- eth/tracers/js/internal/tracers/data_cleaner.go
+++ eth/tracers/js/internal/tracers/data_cleaner.go
@@ -115,3 +115,25 @@ func main() {
 	moreCleaned := cleaner.Deduplicate(moreData)
 	fmt.Println("More cleaned:", moreCleaned)
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 2, 3, 4, 4, 5}
+	cleaned := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
