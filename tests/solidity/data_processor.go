b1ad21361632f041024b035f62ddd087aec66b5b
heyQuim <hey.quim@hotmail.com>
2026-02-27 23:55:12 +0000

    Add data processing utility for cleaning and validating input strings

--- tests/solidity/data_processor.go
+++ tests/solidity/data_processor.go
@@ -29,3 +29,26 @@ func RemoveExtraSpaces(input string) string {
 	re := regexp.MustCompile(`\s+`)
 	return re.ReplaceAllString(strings.TrimSpace(input), " ")
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(trimmed, " ")
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func ContainsOnlyAlphanumeric(s string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	return re.MatchString(s)
+}
