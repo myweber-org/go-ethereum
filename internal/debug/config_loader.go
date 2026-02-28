c3e7e8db73b7305f47180b4171471cc71fd00628
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-28 15:17:06 +0000

    Add configuration loader with YAML support and environment variable overrides

--- internal/debug/config_loader.go
+++ internal/debug/config_loader.go
@@ -202,3 +202,101 @@ func (c *Config) String() string {
 	data, _ := json.MarshalIndent(c, "", "  ")
 	return string(data)
 }
+package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+		SSLMode  string `yaml:"ssl_mode" env:"DB_SSL_MODE"`
+	} `yaml:"database"`
+
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Format string `yaml:"format" env:"LOG_FORMAT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	overrideWithEnvVars(config)
+
+	return config, nil
+}
+
+func overrideWithEnvVars(config *Config) {
+	overrideStruct(config, "")
+}
+
+func overrideStruct(s interface{}, prefix string) {
+	val := reflect.ValueOf(s).Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+
+		envTag := fieldType.Tag.Get("env")
+		yamlTag := fieldType.Tag.Get("yaml")
+
+		if field.Kind() == reflect.Struct {
+			newPrefix := prefix
+			if yamlTag != "" {
+				newPrefix = strings.TrimSuffix(prefix+"_"+strings.ToUpper(yamlTag), "_")
+			}
+			overrideStruct(field.Addr().Interface(), newPrefix)
+			continue
+		}
+
+		if envTag == "" {
+			continue
+		}
+
+		envKey := envTag
+		if prefix != "" {
+			envKey = prefix + "_" + envKey
+		}
+
+		if envValue := os.Getenv(envKey); envValue != "" {
+			switch field.Kind() {
+			case reflect.String:
+				field.SetString(envValue)
+			case reflect.Int:
+				if intValue, err := strconv.Atoi(envValue); err == nil {
+					field.SetInt(int64(intValue))
+				}
+			case reflect.Bool:
+				if boolValue, err := strconv.ParseBool(envValue); err == nil {
+					field.SetBool(boolValue)
+				}
+			}
+		}
+	}
+}
