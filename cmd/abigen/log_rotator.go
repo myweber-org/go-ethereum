2feb0d506b8f5aeeadb61b45acf015e09f9999f7
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-28 03:05:00 +0000

    Implement log rotation with size-based file splitting and compression

--- cmd/abigen/log_rotator.go
+++ cmd/abigen/log_rotator.go
@@ -147,3 +147,125 @@ func main() {
 
 	fmt.Println("Log rotation test completed")
 }
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+const (
+	maxFileSize = 10 * 1024 * 1024 // 10MB
+	backupCount = 5
+)
+
+type RotatingLogger struct {
+	mu         sync.Mutex
+	file       *os.File
+	size       int64
+	basePath   string
+	currentNum int
+}
+
+func NewRotatingLogger(path string) (*RotatingLogger, error) {
+	rl := &RotatingLogger{
+		basePath: path,
+	}
+	if err := rl.rotateIfNeeded(); err != nil {
+		return nil, err
+	}
+	return rl, nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if err := rl.rotateIfNeeded(); err != nil {
+		return 0, err
+	}
+
+	n, err := rl.file.Write(p)
+	rl.size += int64(n)
+	return n, err
+}
+
+func (rl *RotatingLogger) rotateIfNeeded() error {
+	if rl.file != nil && rl.size < maxFileSize {
+		return nil
+	}
+
+	if rl.file != nil {
+		rl.file.Close()
+		if err := rl.compressCurrent(); err != nil {
+			return err
+		}
+		rl.cleanOldBackups()
+	}
+
+	newFile, err := os.OpenFile(rl.basePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	stat, err := newFile.Stat()
+	if err != nil {
+		newFile.Close()
+		return err
+	}
+
+	rl.file = newFile
+	rl.size = stat.Size()
+	rl.currentNum = 0
+	return nil
+}
+
+func (rl *RotatingLogger) compressCurrent() error {
+	src, err := os.Open(rl.basePath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	backupName := fmt.Sprintf("%s.%d.gz", rl.basePath, time.Now().Unix())
+	dst, err := os.Create(backupName)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	gz := gzip.NewWriter(dst)
+	defer gz.Close()
+
+	_, err = io.Copy(gz, src)
+	return err
+}
+
+func (rl *RotatingLogger) cleanOldBackups() {
+	pattern := rl.basePath + ".*.gz"
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return
+	}
+
+	if len(matches) > backupCount {
+		for i := 0; i < len(matches)-backupCount; i++ {
+			os.Remove(matches[i])
+		}
+	}
+}
+
+func (rl *RotatingLogger) Close() error {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if rl.file != nil {
+		return rl.file.Close()
+	}
+	return nil
+}
