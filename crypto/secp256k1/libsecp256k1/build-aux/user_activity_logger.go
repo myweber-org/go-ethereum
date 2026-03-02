c79cd4c51c12b46fd1afe068bbf3a3f6523cca9f
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-02 20:04:13 +0000

    Add user activity logging middleware for tracking API usage

--- crypto/secp256k1/libsecp256k1/build-aux/user_activity_logger.go
+++ crypto/secp256k1/libsecp256k1/build-aux/user_activity_logger.go
@@ -37,4 +37,38 @@ type responseWriter struct {
 func (rw *responseWriter) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IPAddress string
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		activity := ActivityLog{
+			Timestamp: time.Now().UTC(),
+			Method:    r.Method,
+			Path:      r.URL.Path,
+			UserAgent: r.UserAgent(),
+			IPAddress: r.RemoteAddr,
+		}
+
+		log.Printf("Activity: %s %s from %s (%s)", 
+			activity.Method, 
+			activity.Path, 
+			activity.IPAddress, 
+			activity.UserAgent)
+
+		next.ServeHTTP(w, r)
+	})
 }
