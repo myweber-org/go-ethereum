f816c6515d4f304e0bfe10144a429be2b11097b5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-03 19:46:20 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- core/asm/data_cleaner.go
+++ core/asm/data_cleaner.go
@@ -83,3 +83,26 @@ func main() {
 		fmt.Printf("Email %s valid: %v\n", email, cleaner.ValidateEmail(email))
 	}
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Printf("Original: %v\n", data)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
