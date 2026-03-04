8e945a74c4e2fab6e23248ce467fb7b47fdb7b5c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-04 00:15:26 +0000

    Add user activity logging with timestamp and action type

--- build/deb/ethereum/completions/user_activity_logger.go
+++ build/deb/ethereum/completions/user_activity_logger.go
@@ -35,4 +35,49 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details"`
+}
+
+func logActivity(userID, action, details string) error {
+    logEntry := ActivityLog{
+        Timestamp: time.Now().UTC(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    if err := encoder.Encode(logEntry); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    if err := logActivity("user123", "LOGIN", "User logged in from web browser"); err != nil {
+        fmt.Printf("Failed to log activity: %v\n", err)
+        return
+    }
+    fmt.Println("Activity logged successfully")
 }
