e58eb1f9832d1926bca4d47b4fd9e46a812a544e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-04 02:30:28 +0000

    Add user activity logging middleware for API endpoints

--- consensus/user_activity_logger.go
+++ consensus/user_activity_logger.go
@@ -16,116 +16,14 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	userAgent := r.Header.Get("User-Agent")
-	ipAddress := r.RemoteAddr
-	requestPath := r.URL.Path
-
 	al.handler.ServeHTTP(w, r)
-
 	duration := time.Since(start)
-	log.Printf("Activity: %s %s from %s (User-Agent: %s) took %v",
-		r.Method, requestPath, ipAddress, userAgent, duration)
-}package middleware
 
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "time"
-)
-
-type ActivityLog struct {
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Timestamp time.Time `json:"timestamp"`
-    Details   string    `json:"details,omitempty"`
-}
-
-func logActivity(userID, action, details string) ActivityLog {
-    logEntry := ActivityLog{
-        UserID:    userID,
-        Action:    action,
-        Timestamp: time.Now().UTC(),
-        Details:   details,
-    }
-    
-    return logEntry
-}
-
-func saveLogToFile(logEntry ActivityLog, filename string) error {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-    
-    encoder := json.NewEncoder(file)
-    encoder.SetIndent("", "  ")
-    
-    if err := encoder.Encode(logEntry); err != nil {
-        return err
-    }
-    
-    return nil
-}
-
-func main() {
-    logEntry := logActivity("user123", "login", "User logged in from web browser")
-    
-    if err := saveLogToFile(logEntry, "activity_log.json"); err != nil {
-        fmt.Printf("Error saving log: %v\n", err)
-        return
-    }
-    
-    fmt.Printf("Activity logged: %s performed %s at %s\n", 
-        logEntry.UserID, logEntry.Action, logEntry.Timestamp.Format(time.RFC3339))
+	log.Printf(
+		"User activity: %s %s from %s completed in %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
