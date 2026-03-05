502d8760ac6caec3502a88c92494463df0c4b943
VioletInquiry <violet.inquiry@outlook.com>
2026-03-05 23:21:40 +0000

    Add utility to clean up old temporary files in a directory

--- accounts/keystore/testdata/v1/file_cleaner.go
+++ accounts/keystore/testdata/v1/file_cleaner.go
@@ -33,4 +33,49 @@ func main() {
 	}
 
 	fmt.Printf("Cleaned %d temporary files older than 7 days\n", removedCount)
+}package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	maxAgeHours = 24 * 7 // 1 week
+)
+
+func main() {
+	dir := os.TempDir()
+	fmt.Printf("Cleaning temporary files older than %d hours in: %s\n", maxAgeHours, dir)
+
+	err := cleanOldFiles(dir)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Cleanup completed successfully.")
+}
+
+func cleanOldFiles(dirPath string) error {
+	cutoffTime := time.Now().Add(-time.Hour * maxAgeHours)
+
+	return filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+
+		return nil
+	})
 }
