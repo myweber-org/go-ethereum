4e63d5718c0ad257161abda1880275ff6fa2680a
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-05 20:51:53 +0000

    Add utility to remove temporary files older than 7 days

--- cmd/devp2p/internal/v4test/file_cleaner.go
+++ cmd/devp2p/internal/v4test/file_cleaner.go
@@ -32,4 +32,52 @@ func main() {
 	}
 
 	fmt.Printf("Cleaned up %d old temporary files.\n", removedCount)
+}package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	retentionDays = 7
+)
+
+func main() {
+	err := cleanOldFiles(tempDir, retentionDays)
+	if err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string, days int) error {
+	cutoffTime := time.Now().AddDate(0, 0, -days)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+
+		return nil
+	})
 }
