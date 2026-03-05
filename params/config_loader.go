4804392361799cce09bb9b8ad012392eaeb27fbc
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 00:22:21 +0000

    Add configuration loader with YAML support and environment variable overrides

--- params/config_loader.go
+++ params/config_loader.go
@@ -325,4 +325,78 @@ func ValidateConfig(config *AppConfig) bool {
 		return false
 	}
 	return true
+}package config
+
+import (
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Username string `yaml:"username" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASS"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(data, config); err != nil {
+		return nil, err
+	}
+
+	config.overrideFromEnv()
+
+	return config, nil
+}
+
+func (c *Config) overrideFromEnv() {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		c.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		port := 0
+		fmt.Sscanf(val, "%d", &port)
+		if port > 0 {
+			c.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		c.Database.Host = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		port := 0
+		fmt.Sscanf(val, "%d", &port)
+		if port > 0 {
+			c.Database.Port = port
+		}
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		c.Database.Username = val
+	}
+	if val := os.Getenv("DB_PASS"); val != "" {
+		c.Database.Password = val
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		c.Database.Name = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		c.LogLevel = val
+	}
 }
