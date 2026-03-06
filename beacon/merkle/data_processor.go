802e2bef6de1ded2fca061d0cf1013c81a46acbf
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-06 22:34:19 +0000

    Add data processing utility for cleaning and validating input strings

--- beacon/merkle/data_processor.go
+++ beacon/merkle/data_processor.go
@@ -64,3 +64,41 @@ func CalculateAverage(records []DataRecord) float64 {
 	}
 	return sum / float64(len(records))
 }
+package data
+
+import (
+	"regexp"
+	"strings"
+)
+
+type Processor struct {
+	allowedPattern *regexp.Regexp
+}
+
+func NewProcessor(allowedPattern string) (*Processor, error) {
+	compiled, err := regexp.Compile(allowedPattern)
+	if err != nil {
+		return nil, err
+	}
+	return &Processor{allowedPattern: compiled}, nil
+}
+
+func (p *Processor) CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return p.allowedPattern.FindString(trimmed)
+}
+
+func (p *Processor) Validate(input string) bool {
+	return p.allowedPattern.MatchString(input)
+}
+
+func (p *Processor) ProcessBatch(inputs []string) []string {
+	var results []string
+	for _, input := range inputs {
+		cleaned := p.CleanInput(input)
+		if cleaned != "" {
+			results = append(results, cleaned)
+		}
+	}
+	return results
+}
