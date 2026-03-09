c0dd60f5b060a98921307895dd75ca35b1129510
ElaraWrites <elara.writes@outlook.com>
2026-03-09 00:20:48 +0000

    Add configuration loader with YAML parsing support

--- core/rawdb/ancienttest/config_loader.go
+++ core/rawdb/ancienttest/config_loader.go
@@ -61,4 +61,60 @@ func validateConfig(cfg *Config) error {
 		cfg.LogLevel = "info"
 	}
 	return nil
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+
+    "gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+    Host     string `yaml:"host"`
+    Port     int    `yaml:"port"`
+    Username string `yaml:"username"`
+    Password string `yaml:"password"`
+    Name     string `yaml:"name"`
+}
+
+type ServerConfig struct {
+    Port int    `yaml:"port"`
+    Env  string `yaml:"env"`
+}
+
+type Config struct {
+    Database DatabaseConfig `yaml:"database"`
+    Server   ServerConfig   `yaml:"server"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config Config
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    if config.Server.Env == "" {
+        config.Server.Env = "development"
+    }
+
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+
+    return &config, nil
+}
+
+func LoadConfigFromEnv() (*Config, error) {
+    configPath := os.Getenv("CONFIG_PATH")
+    if configPath == "" {
+        configPath = "config.yaml"
+    }
+    return LoadConfig(configPath)
 }
