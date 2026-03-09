5ff5e802f1778115a39480faac798b3420e5bd44
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-09 20:34:14 +0000

    Add file encryption utility with AES-256-GCM implementation

--- crypto/signify/file_encryptor.go
+++ crypto/signify/file_encryptor.go
@@ -134,3 +134,104 @@ func main() {
 		os.Exit(1)
 	}
 }
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptData(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func generateKey() ([]byte, error) {
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_encryptor <encrypt|decrypt>")
+		os.Exit(1)
+	}
+
+	mode := os.Args[1]
+	key, _ := generateKey()
+	keyHex := hex.EncodeToString(key)
+	fmt.Printf("Generated key: %s\n", keyHex)
+
+	sampleData := []byte("Sensitive information requiring encryption")
+
+	switch mode {
+	case "encrypt":
+		encrypted, err := encryptData(sampleData, key)
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Encrypted data (hex): %s\n", hex.EncodeToString(encrypted))
+
+	case "decrypt":
+		encrypted, _ := encryptData(sampleData, key)
+		decrypted, err := decryptData(encrypted, key)
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Decrypted data: %s\n", string(decrypted))
+
+	default:
+		fmt.Println("Invalid mode. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
