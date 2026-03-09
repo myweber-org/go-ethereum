de4e24dd702dfc283f991bbb40c2cb645108017d
ElaraWrites <elara.writes@outlook.com>
2026-03-09 16:32:06 +0000

    Add utility to remove duplicate lines from a text file

--- signer/storage/file_cleaner.go
+++ signer/storage/file_cleaner.go
@@ -25,4 +25,63 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error cleaning temp directory: %v\n", err)
 	}
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func removeDuplicates(inputPath, outputPath string) error {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	seen := make(map[string]bool)
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !seen[line] {
+			seen[line] = true
+			lines = append(lines, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	writer := bufio.NewWriter(outFile)
+	for _, line := range lines {
+		fmt.Fprintln(writer, line)
+	}
+	return writer.Flush()
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: file_cleaner <input_file> <output_file>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	if err := removeDuplicates(inputFile, outputFile); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Duplicates removed. Output written to %s\n", outputFile)
 }
