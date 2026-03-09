58356f9822969902188c50feeea72a8013ee3078
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-09 15:43:55 +0000

    Add user activity logging middleware to track API usage

--- common/mclock/user_activity_logger.go
+++ common/mclock/user_activity_logger.go
@@ -6,47 +6,6 @@ import (
 	"time"
 )
 
-type ActivityLog struct {
-	UserID    string
-	IPAddress string
-	Method    string
-	Path      string
-	Timestamp time.Time
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserIDFromToken(authHeader)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			IPAddress: r.RemoteAddr,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			Timestamp: start,
-		}
-
-		log.Printf("Activity: %s %s by %s from %s", activity.Method, activity.Path, activity.UserID, activity.IPAddress)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extractUserIDFromToken(token string) string {
-	return "user_" + token[:8]
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
 type ActivityLogger struct {
 	handler http.Handler
 }
@@ -57,11 +16,12 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+	
 	al.handler.ServeHTTP(w, r)
+	
 	duration := time.Since(start)
-
-	log.Printf(
-		"Activity: %s %s from %s took %v",
+	
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
 		r.RemoteAddr,
