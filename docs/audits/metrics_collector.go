18d24139a7ca4cb7f02d2a6b61e4ca8ab0706b7c
VioletInquiry <violet.inquiry@outlook.com>
2026-03-10 00:33:43 +0000

    Add system metrics collection with CPU and memory usage reporting

--- docs/audits/metrics_collector.go
+++ docs/audits/metrics_collector.go
@@ -61,4 +61,51 @@ func main() {
 
     wrappedMux := metricsMiddleware(mux)
     http.ListenAndServe(":8080", wrappedMux)
+}package main
+
+import (
+    "fmt"
+    "runtime"
+    "time"
+)
+
+type SystemMetrics struct {
+    Timestamp     time.Time
+    CPUPercent    float64
+    MemoryUsedMB  uint64
+    MemoryTotalMB uint64
+    GoroutineCount int
+}
+
+func collectMetrics() SystemMetrics {
+    var memStats runtime.MemStats
+    runtime.ReadMemStats(&memStats)
+
+    return SystemMetrics{
+        Timestamp:     time.Now(),
+        MemoryUsedMB:  memStats.Alloc / 1024 / 1024,
+        MemoryTotalMB: memStats.Sys / 1024 / 1024,
+        GoroutineCount: runtime.NumGoroutine(),
+    }
+}
+
+func printMetrics(metrics SystemMetrics) {
+    fmt.Printf("[%s] Goroutines: %d | Memory: %dMB/%dMB\n",
+        metrics.Timestamp.Format("15:04:05"),
+        metrics.GoroutineCount,
+        metrics.MemoryUsedMB,
+        metrics.MemoryTotalMB)
+}
+
+func main() {
+    ticker := time.NewTicker(5 * time.Second)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ticker.C:
+            metrics := collectMetrics()
+            printMetrics(metrics)
+        }
+    }
 }
