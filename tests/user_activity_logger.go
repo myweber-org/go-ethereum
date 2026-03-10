446f52ec969d7c59672f551fd584c09d7ecc88bf
heyQuim <hey.quim@hotmail.com>
2026-03-10 22:42:51 +0000

    Add user activity logging middleware for API endpoints

--- tests/user_activity_logger.go
+++ tests/user_activity_logger.go
@@ -89,3 +89,50 @@ func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
 }
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := r.Header.Get("X-User-ID")
+	if userID == "" {
+		userID = "anonymous"
+	}
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s accessed %s %s from %s - Duration: %v",
+		userID, r.Method, r.URL.Path, r.RemoteAddr, duration)
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/data", apiHandler)
+
+	wrappedMux := NewActivityLogger(mux)
+
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", wrappedMux); err != nil {
+		log.Fatal(err)
+	}
+}
