d207420742085ab7fe3876ab409bfc12056196cd
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-10 19:20:34 +0000

    Add user activity logging with timestamp and action type

--- cmd/clef/testdata/user_activity_logger.go
+++ cmd/clef/testdata/user_activity_logger.go
@@ -44,4 +44,71 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type Activity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+    encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{
+        logFile: file,
+        encoder: json.NewEncoder(file),
+    }, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, action, details string) error {
+    activity := Activity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+    return l.encoder.Encode(activity)
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.jsonl")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user123", "file_upload", "uploaded profile.jpg")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user456", "logout", "")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
 }
