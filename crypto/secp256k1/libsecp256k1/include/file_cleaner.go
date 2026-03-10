5b50613fe6b608dfcf2a5f00a4b5a511ed95a946
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-10 23:00:22 +0000

    Add utility to remove temporary files older than 7 days

--- crypto/secp256k1/libsecp256k1/include/file_cleaner.go
+++ crypto/secp256k1/libsecp256k1/include/file_cleaner.go
@@ -53,4 +53,57 @@ func main() {
 	writer.Flush()
 
 	fmt.Printf("Cleaned file saved as: %s\n", outputFile)
+}package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_temp"
+	daysToKeep   = 7
+	fileExt      = ".tmp"
+)
+
+func main() {
+	err := cleanOldFiles(tempDir, daysToKeep, fileExt)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string, days int, extension string) error {
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return fmt.Errorf("failed to read directory: %w", err)
+	}
+
+	cutoffTime := time.Now().AddDate(0, 0, -days)
+	removedCount := 0
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) != extension {
+			continue
+		}
+
+		if file.ModTime().Before(cutoffTime) {
+			fullPath := filepath.Join(dirPath, file.Name())
+			err := os.Remove(fullPath)
+			if err != nil {
+				fmt.Printf("Warning: Could not remove %s: %v\n", fullPath, err)
+				continue
+			}
+			removedCount++
+			fmt.Printf("Removed: %s (modified: %v)\n", file.Name(), file.ModTime())
+		}
+	}
+
+	fmt.Printf("Total files removed: %d\n", removedCount)
+	return nil
 }
