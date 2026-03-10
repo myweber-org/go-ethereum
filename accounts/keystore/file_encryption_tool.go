6fa51aa2bf48e5e5114eea9b234f79c64a5f9448
heyQuim <hey.quim@hotmail.com>
2026-03-10 01:32:14 +0000

    Add AES-256 file encryption utility with secure key derivation

--- accounts/keystore/file_encryption_tool.go
+++ accounts/keystore/file_encryption_tool.go
@@ -106,4 +106,121 @@ func main() {
 
 	fmt.Printf("Key (hex): %x\n", key)
 	fmt.Println("Operation completed successfully")
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, passphrase string) error {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey(passphrase, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return err
+	}
+
+	inputData, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	encryptedData := gcm.Seal(nil, nonce, inputData, nil)
+
+	outputData := append(salt, nonce...)
+	outputData = append(outputData, encryptedData...)
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func decryptFile(inputPath, outputPath, passphrase string) error {
+	inputData, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(inputData) < 48 {
+		return errors.New("invalid encrypted file format")
+	}
+
+	salt := inputData[:16]
+	nonce := inputData[16:32]
+	ciphertext := inputData[32:]
+
+	key := deriveKey(passphrase, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: go run file_encryption_tool.go <encrypt|decrypt> <input> <output> <passphrase>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	passphrase := os.Args[4]
+
+	var err error
+	switch operation {
+	case "encrypt":
+		err = encryptFile(inputPath, outputPath, passphrase)
+	case "decrypt":
+		err = decryptFile(inputPath, outputPath, passphrase)
+	default:
+		err = errors.New("invalid operation, use 'encrypt' or 'decrypt'")
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Operation completed successfully")
 }
