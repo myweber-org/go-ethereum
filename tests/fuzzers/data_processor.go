fa08d2df8d9c7aae24c0b56f75c7b274ebefef0b
FelixMakes <felix.makes@hotmail.com>
2026-03-10 02:55:16 +0000

    Add data processing function to calculate average from slice of integers

--- tests/fuzzers/data_processor.go
+++ tests/fuzzers/data_processor.go
@@ -188,4 +188,25 @@ func ProcessData(inputs []string) []string {
 		}
 	}
 	return results
+}package main
+
+import "fmt"
+
+func calculateAverage(numbers []int) float64 {
+    if len(numbers) == 0 {
+        return 0.0
+    }
+    
+    sum := 0
+    for _, num := range numbers {
+        sum += num
+    }
+    
+    return float64(sum) / float64(len(numbers))
+}
+
+func main() {
+    data := []int{10, 20, 30, 40, 50}
+    avg := calculateAverage(data)
+    fmt.Printf("Average: %.2f\n", avg)
 }
