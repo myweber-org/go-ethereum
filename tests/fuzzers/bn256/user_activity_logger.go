f9dbf38857f9891d78af54f1816f6fd0846ab0ca
heyQuim <hey.quim@hotmail.com>
2026-03-10 23:04:49 +0000

    Add user activity logging middleware for API endpoints

--- tests/fuzzers/bn256/user_activity_logger.go
+++ tests/fuzzers/bn256/user_activity_logger.go
@@ -47,4 +47,38 @@ func main() {
     }
 
     fmt.Println("Activity logging completed")
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ip := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s %s %s completed in %v", userID, ip, method, path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if user := r.Header.Get("X-User-ID"); user != "" {
+		return user
+	}
+	return "anonymous"
 }
