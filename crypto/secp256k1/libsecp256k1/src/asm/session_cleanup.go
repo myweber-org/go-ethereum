42986db8e06244491ea3025bbf994b781321b05f
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-11 16:35:48 +0000

    Add session cleanup job to delete expired sessions from Redis

--- crypto/secp256k1/libsecp256k1/src/asm/session_cleanup.go
+++ crypto/secp256k1/libsecp256k1/src/asm/session_cleanup.go
@@ -41,4 +41,94 @@ func cleanupExpiredSessions(db *database.DB) {
 
 	rows, _ := result.RowsAffected()
 	log.Printf("Cleaned up %d expired sessions", rows)
+}package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+const (
+	sessionKeyPattern = "session:*"
+	batchSize         = 100
+	scanInterval      = 1 * time.Hour
+)
+
+type SessionCleaner struct {
+	client *redis.Client
+}
+
+func NewSessionCleaner(addr string) *SessionCleaner {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+	return &SessionCleaner{client: rdb}
+}
+
+func (sc *SessionCleaner) CleanExpiredSessions(ctx context.Context) error {
+	var cursor uint64
+	var deletedCount int
+
+	for {
+		keys, nextCursor, err := sc.client.Scan(ctx, cursor, sessionKeyPattern, batchSize).Result()
+		if err != nil {
+			return fmt.Errorf("scan error: %w", err)
+		}
+
+		for _, key := range keys {
+			ttl, err := sc.client.TTL(ctx, key).Result()
+			if err != nil {
+				log.Printf("failed to get TTL for key %s: %v", key, err)
+				continue
+			}
+
+			if ttl < 0 {
+				if err := sc.client.Del(ctx, key).Err(); err != nil {
+					log.Printf("failed to delete expired session %s: %v", key, err)
+				} else {
+					deletedCount++
+				}
+			}
+		}
+
+		if nextCursor == 0 {
+			break
+		}
+		cursor = nextCursor
+	}
+
+	log.Printf("cleaned %d expired sessions", deletedCount)
+	return nil
+}
+
+func (sc *SessionCleaner) RunPeriodicCleanup(ctx context.Context) {
+	ticker := time.NewTicker(scanInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			if err := sc.CleanExpiredSessions(ctx); err != nil {
+				log.Printf("session cleanup failed: %v", err)
+			}
+		case <-ctx.Done():
+			log.Println("session cleanup stopped")
+			return
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	cleaner := NewSessionCleaner("localhost:6379")
+
+	go cleaner.RunPeriodicCleanup(ctx)
+
+	select {}
 }
