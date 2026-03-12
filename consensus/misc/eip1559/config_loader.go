aa92eefb0f5907cd003bb304ddfafe0a7b59ebd8
FelixMakes <felix.makes@hotmail.com>
2026-03-12 00:55:54 +0000

    Add configuration loader with YAML support and environment variable overrides

--- consensus/misc/eip1559/config_loader.go
+++ consensus/misc/eip1559/config_loader.go
@@ -154,4 +154,91 @@ func (c *Config) validate() error {
 	}
 
 	return nil
+}package config
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+
+    "gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+    Host     string `yaml:"host" env:"DB_HOST"`
+    Port     int    `yaml:"port" env:"DB_PORT"`
+    Username string `yaml:"username" env:"DB_USER"`
+    Password string `yaml:"password" env:"DB_PASS"`
+    Name     string `yaml:"name" env:"DB_NAME"`
+}
+
+type ServerConfig struct {
+    Port         int    `yaml:"port" env:"SERVER_PORT"`
+    ReadTimeout  int    `yaml:"read_timeout" env:"READ_TIMEOUT"`
+    WriteTimeout int    `yaml:"write_timeout" env:"WRITE_TIMEOUT"`
+    Debug        bool   `yaml:"debug" env:"DEBUG"`
+}
+
+type Config struct {
+    Database DatabaseConfig `yaml:"database"`
+    Server   ServerConfig   `yaml:"server"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+    if configPath == "" {
+        configPath = "config.yaml"
+    }
+
+    absPath, err := filepath.Abs(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to get absolute path: %w", err)
+    }
+
+    data, err := os.ReadFile(absPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config Config
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    loadFromEnv(&config)
+
+    return &config, nil
+}
+
+func loadFromEnv(config *Config) {
+    setFromEnv(&config.Database.Host, "DB_HOST")
+    setFromEnvInt(&config.Database.Port, "DB_PORT")
+    setFromEnv(&config.Database.Username, "DB_USER")
+    setFromEnv(&config.Database.Password, "DB_PASS")
+    setFromEnv(&config.Database.Name, "DB_NAME")
+
+    setFromEnvInt(&config.Server.Port, "SERVER_PORT")
+    setFromEnvInt(&config.Server.ReadTimeout, "READ_TIMEOUT")
+    setFromEnvInt(&config.Server.WriteTimeout, "WRITE_TIMEOUT")
+    setFromEnvBool(&config.Server.Debug, "DEBUG")
+}
+
+func setFromEnv(field *string, envVar string) {
+    if val := os.Getenv(envVar); val != "" {
+        *field = val
+    }
+}
+
+func setFromEnvInt(field *int, envVar string) {
+    if val := os.Getenv(envVar); val != "" {
+        var intVal int
+        if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+            *field = intVal
+        }
+    }
+}
+
+func setFromEnvBool(field *bool, envVar string) {
+    if val := os.Getenv(envVar); val != "" {
+        *field = val == "true" || val == "1" || val == "yes"
+    }
 }
