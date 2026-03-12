dccb00e6ffdd1c04e93a448ed82d961778a765ca
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-12 16:04:05 +0000

    Add user activity logging middleware for tracking API usage

--- graphql/user_activity_logger.go
+++ graphql/user_activity_logger.go
@@ -25,4 +25,32 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"Method: %s | Path: %s | RemoteAddr: %s | Duration: %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
