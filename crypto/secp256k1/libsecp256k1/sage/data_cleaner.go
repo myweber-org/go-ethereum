39aa2f15a47555c42393cdc61ee14d4506c2788b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-12 16:33:13 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- crypto/secp256k1/libsecp256k1/sage/data_cleaner.go
+++ crypto/secp256k1/libsecp256k1/sage/data_cleaner.go
@@ -78,3 +78,64 @@ func main() {
 
 	fmt.Printf("Successfully cleaned data. Output saved to %s\n", outputFile)
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: data_cleaner <input_file.csv>")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := strings.TrimSuffix(inputFile, ".csv") + "_cleaned.csv"
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error reading CSV: %v\n", err)
+		return
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		key := strings.Join(record, "|")
+		if !seen[key] {
+			seen[key] = true
+			uniqueRecords = append(uniqueRecords, record)
+		}
+	}
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		return
+	}
+	defer outFile.Close()
+
+	writer := csv.NewWriter(outFile)
+	err = writer.WriteAll(uniqueRecords)
+	if err != nil {
+		fmt.Printf("Error writing CSV: %v\n", err)
+		return
+	}
+
+	writer.Flush()
+	fmt.Printf("Cleaned data saved to: %s\n", outputFile)
+	fmt.Printf("Removed %d duplicate rows\n", len(records)-len(uniqueRecords))
+}
