fa40966abb212b00f007566cb99d825bd6773a1f
InkspillVerse <inkspill.verse@outlook.com>
2026-03-13 16:34:37 +0000

    Add user activity logging middleware for tracking API usage

--- triedb/pathdb/user_activity_logger.go
+++ triedb/pathdb/user_activity_logger.go
@@ -51,4 +51,46 @@ func (al *ActivityLogger) Cleanup() {
 			}
 		}
 	}()
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		userAgent := r.Header.Get("User-Agent")
+		clientIP := r.RemoteAddr
+
+		al.Logger.Printf("Request started: %s %s from %s (UA: %s)", 
+			r.Method, r.URL.Path, clientIP, userAgent)
+
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+		al.Logger.Printf("Request completed: %s %s - Status: %d - Duration: %v", 
+			r.Method, r.URL.Path, rw.statusCode, duration)
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
