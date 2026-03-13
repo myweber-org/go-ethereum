1a543e5319839a83d10961258b96fb22d0bbf016
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-13 16:52:20 +0000

    Add user activity logging with timestamps and event types

--- crypto/secp256k1/libsecp256k1/src/modules/recovery/user_activity_logger.go
+++ crypto/secp256k1/libsecp256k1/src/modules/recovery/user_activity_logger.go
@@ -1,49 +1,75 @@
-package middleware
+package main
 
 import (
-	"log"
-	"net/http"
-	"time"
+    "encoding/json"
+    "fmt"
+    "os"
+    "time"
 )
 
+type ActivityEvent struct {
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details"`
+}
+
 type ActivityLogger struct {
-	Logger *log.Logger
+    logFile *os.File
 }
 
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"%s %s %d %s %s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-		)
-	})
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+    event := ActivityEvent{
+        UserID:    userID,
+        EventType: eventType,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    data = append(data, '\n')
+    _, err = l.logFile.Write(data)
+    return err
 }
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
 }
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+func main() {
+    logger, err := NewActivityLogger("activity.log")
+    if err != nil {
+        fmt.Printf("Failed to create logger: %v\n", err)
+        return
+    }
+    defer logger.Close()
+
+    events := []struct {
+        userID, eventType, details string
+    }{
+        {"user123", "login", "User logged in from Chrome browser"},
+        {"user123", "view_page", "Viewed dashboard page"},
+        {"user456", "purchase", "Purchased item SKU-789"},
+        {"user123", "logout", "User logged out after 15 minutes"},
+    }
+
+    for _, e := range events {
+        if err := logger.LogActivity(e.userID, e.eventType, e.details); err != nil {
+            fmt.Printf("Failed to log activity: %v\n", err)
+        }
+    }
+
+    fmt.Println("Activity logging completed successfully")
 }
